a8ce74097f8419a45033fd676a57ffb723a5839b
Roberto R. Exposito <rreye@udc.es>
2020-03-05 10:48:01 +0100

    Increase delay time when starting the master

--- solutions/src/Spark/bin/start.sh
+++ solutions/src/Spark/bin/start.sh
@@ -4,7 +4,7 @@ $COMMON_SRC_DIR/bin/start_hadoop_yarn.sh
 
 $SPARK_HOME/sbin/start-master.sh &
 
-sleep 2
+sleep 5
 
 $SPARK_HOME/sbin/start-slaves.sh &
 
@@ -17,4 +17,4 @@ then
 	$SPARK_HOME/sbin/start-history-server.sh &
 fi
 
-sleep 15
+sleep 10
