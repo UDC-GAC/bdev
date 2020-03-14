4ead9064f12a87d03bd289ccd292bc5e13a75bc7
Roberto R. Exposito <rreye@udc.es>
2020-03-14 18:08:20 +0100

    Disable HDFS short-circuit reads by default

--- etc/hdfs-default.sh
+++ etc/hdfs-default.sh
@@ -5,5 +5,5 @@
 export BLOCKSIZE=$((128*1024*1024)) 	# HDFS block size (Bytes)
 export REPLICATION_FACTOR=3		# Number of block replications
 export NAMENODE_HANDLER_COUNT=50	# Number of NameNode RPC server threads: ln(#DataNodes)*20 (eg, 50 threads for 12 DataNodes)
-export SHORT_CIRCUIT_LOCAL_READS=true	# Enable short-circuit local reads to bypass the DataNode, allowing the client to read the file directly
+export SHORT_CIRCUIT_LOCAL_READS=false	# Enable short-circuit local reads to bypass the DataNode, allowing the client to read the file directly
 export DOMAIN_SOCKET_PATH=/var/lib/hdfs/$USER	# Permissions in this special path must be properly configured before enabling the short-circuit reads
