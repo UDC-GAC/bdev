6e9bdd2d5c5b67770bdf6152e5d7a3774e9661c5
Roberto R. Exposito <rreye@udc.es>
2020-03-14 18:07:28 +0100

    Add support for short-circuit local reads in HDFS

--- experiment/hdfs-conf.sh
+++ experiment/hdfs-conf.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 ## Configuration parameters corresponding with the hdfs-site.xml file of Hadoop configuration
-
-# 
-# export BLOCKSIZE=$((256*1024*1024)) 	# HDFS block size (Bytes)
-# export REPLICATION_FACTOR=3 	# Number of block replications
-# export NAMENODE_HANDLER_COUNT=50 # Number of NameNode RPC server threads: ln(#DataNodes)*20 (eg, 50 threads for 12 DataNodes)
 #
+#export BLOCKSIZE=$((128*1024*1024))     # HDFS block size (Bytes)
+#export REPLICATION_FACTOR=3             # Number of block replications
+#export NAMENODE_HANDLER_COUNT=50        # Number of NameNode RPC server threads: ln(#DataNodes)*20 (eg, 50 threads for 12 DataNodes)
+#export SHORT_CIRCUIT_LOCAL_READS=false  # Enable short-circuit local reads to bypass the DataNode, allowing the client to read the file directly
+#export DOMAIN_SOCKET_PATH=/var/lib/hdfs/$USER   # Permissions in this special path must be properly configured before enabling the short-circuit reads
