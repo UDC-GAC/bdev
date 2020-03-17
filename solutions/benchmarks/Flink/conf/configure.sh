b06452b8840a57512af05f668877ac65e8381a06
Roberto R. Exposito <rreye@udc.es>
2020-03-17 20:55:01 +0100

    Download required jar files for Spark & Flink benchmarks on demand

--- solutions/benchmarks/Flink/conf/configure.sh
+++ solutions/benchmarks/Flink/conf/configure.sh
@@ -3,3 +3,17 @@
 export FLINK_BENCH_JAR_NAME=flinkbench-assembly-1.8.jar
 export FLINK_BENCH_JAR=$SOL_BENCH_DIR/bin/$FLINK_BENCH_JAR_NAME
 export SORT_PARTITIONS=$FLINK_PARALLELISM
+
+if [[ ! -f $FLINK_BENCH_JAR ]]
+then
+        # Download flinkbench jar file
+        URL="http://bdev.des.udc.es/dist/flinkbench"
+        m_echo "Downloading $FLINK_BENCH_JAR_NAME"
+
+        wget -q -O $FLINK_BENCH_JAR $URL/$FLINK_BENCH_JAR_NAME
+
+        if [[ $? != 0 ]]
+        then
+                m_exit "Error when downloading $FLINK_BENCH_JAR_NAME"
+        fi
+fi
--- solutions/benchmarks/Spark/conf/configure.sh
+++ solutions/benchmarks/Spark/conf/configure.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 
-export SPARK_BENCH_JAR_NAME=sparkbench-assembly-2.0.jar
-export SPARK_BENCH_JAR=$SOL_BENCH_DIR/bin/$SPARK_BENCH_JAR_NAME
 export SORT_PARTITIONS=$(($SPARK_EXECUTORS * $SPARK_CORES_PER_EXECUTOR))
+export SPARK_BENCH_JAR_NAME=sparkbench-assembly-2.0.jar
+
+if [[ $SPARK_SERIES == "3" ]]
+then
+	export SPARK_BENCH_JAR_NAME=sparkbench-assembly-3.0.jar
+fi
+
+export SPARK_BENCH_JAR=$SOL_BENCH_DIR/bin/$SPARK_BENCH_JAR_NAME
 export SPARK_BENCH_JAR_DELETE="find $SOLUTION_REPORT_DIR -name "$SPARK_BENCH_JAR_NAME" -type f -delete"
+
+if [[ ! -f $SPARK_BENCH_JAR ]]
+then
+	# Download sparkbench jar file
+	URL="http://bdev.des.udc.es/dist/sparkbench"
+	m_echo "Downloading $SPARK_BENCH_JAR_NAME"
+
+        wget -q -O $SPARK_BENCH_JAR $URL/$SPARK_BENCH_JAR_NAME
+
+        if [[ $? != 0 ]]
+        then
+		m_exit "Error when downloading $SPARK_BENCH_JAR_NAME"
+        fi
+fi
