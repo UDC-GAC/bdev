1670abf116644088401b3597e3806e0a6d0e16c5
Roberto R. Exposito <rreye@udc.es>
2020-08-27 19:10:30 +0200

    Support for configuring waiting time per workload

--- etc/bdev-default.sh
+++ etc/bdev-default.sh
@@ -10,15 +10,14 @@ export ENABLE_OPROFILE=false	# Enable Oprofile event counting
 export ENABLE_BDWATCHDOG=false	# Enable resource monitoring through BDWatchdog
 export DEFAULT_TIMEOUT=86400	# Default workload timeout
 export OUT_DIR=$PWD/${METHOD_NAME}_OUT	# Default report output directory
+export MONITOR_DELAY_SECONDS=10	# Delay time (seconds) after/before starting/stopping all monitors
 
 # Resource stats
 export STAT_GEN_GRAPHS=false	# Generate graphs during execution
-export STAT_SECONDS_INTERVAL=5	# Interval (seconds) for each sample
-export STAT_WAIT_SECONDS=20	# Waiting time (seconds) before each benchmark execution
+export STAT_SECONDS_INTERVAL=2	# Interval (seconds) for each sample
 
 # HPE iLO
-export ILO_SECONDS_INTERVAL=5		# Interval (seconds) for each sample
-export ILO_WAIT_SECONDS=20		# Waiting time (seconds) before each benchmark execution
+export ILO_SECONDS_INTERVAL=2		# Interval (seconds) for each sample
 export ILO_USERNAME="ilo_user"		# User name for ILO interface
 export ILO_PASSWD="..ilo_user.."	# Password for ILO user
 export ILO_BASE_IP=192.168.255		# Base IP for ILO interfaces
@@ -26,13 +25,11 @@ export ILO_MASTER=localhost		# Node which can connect to the ILO interface for a
 
 # RAPL
 export RAPL_GEN_GRAPHS=false	# Generate RAPL graphs during execution
-export RAPL_SECONDS_INTERVAL=5	# Interval (seconds) for each sample
-export RAPL_WAIT_SECONDS=20	# Waiting time (seconds) before each benchmark execution
+export RAPL_SECONDS_INTERVAL=2	# Interval (seconds) for each sample
 
 # OPROFILE
 export OPROFILE_BIN_DIR=`dirname "$(which ocount 2> /dev/null)"`	# Directory containing OProfile binaries
 export OPROFILE_EVENTS="INST_RETIRED,LLC_MISSES,LLC_REFS"	# Events to record during OProfile analysis (can be consulted by using the ophelp command)
-export OPROFILE_WAIT_SECONDS=20		# Waiting time (seconds) before each benchmark execution
 
 # BDWATCHDOG
 export BDWATCHDOG_ATOP=true		# Enable resource monitoring with atop
@@ -40,8 +37,7 @@ export BDWATCHDOG_TURBOSTAT=true	# Enable energy monitoring with turbostat
 export BDWATCHDOG_NETHOGS=true		# Enable network monitoring with nethogs
 export TURBOSTAT_BIN_DIR=`dirname "$(which turbostat 2> /dev/null)"`	# Directory containing turbostat binaries
 export BDWATCHDOG_USERNAME=`echo $USER`	# User name that runs the experiments (useful for the BDWatchdog web interface)
-export BDWATCHDOG_SECONDS_INTERVAL=5	# Interval (seconds) for each sample
-export BDWATCHDOG_WAIT_SECONDS=20	# Waiting time (seconds) before each benchmark execution
+export BDWATCHDOG_SECONDS_INTERVAL=2	# Interval (seconds) for each sample
 export BDWATCHDOG_POST_ENDPOINT="http://hostname:8080/tsdb/api/put"	# OpenTSDB post endpoint
 export BDWATCHDOG_TIMESTAMPING=true   # Enable time stamping service
 export BDWATCHDOG_MONGODB_IP=localhost	# IP/hostname where MongoDB is running
