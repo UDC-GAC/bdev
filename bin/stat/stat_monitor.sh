bb1d76992b810482bef17f55c5dec8275281abc1
Roberto R. Exposito <rreye@udc.es>
2020-08-28 12:48:55 +0200

    Replace dstat with dool, a Python 3 compatible-clone version

--- bin/stat/stat_monitor.sh
+++ bin/stat/stat_monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-${DSTAT} -T -c -C total --load -ms -d --disk-util -fn --output ${STATLOGFILE} --nocolor ${STAT_SECONDS_INTERVAL}
+${DSTAT} -T -c -C total --load -ms -d --disk-util -fn --output ${STATLOGFILE} --noheaders --noupdate ${STAT_SECONDS_INTERVAL}
 
