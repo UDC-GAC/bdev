77b8043fcf8347c70b4bc34f8c300aacd2429528
Roberto R. Exposito <rreye@udc.es>
2020-12-04 11:11:03 +0100

    Sanity check for jps command

--- solutions/src/common/bin/finish_yarn.sh
+++ solutions/src/common/bin/finish_yarn.sh
@@ -3,26 +3,26 @@
 for slave in $SLAVENODES
 do
         m_echo "Finishing NodeManager:" $slave
-        ssh $slave 'for p in $(jps | grep NodeManager | tr -s " " | cut -d " " -f 1);
-        do echo $p; kill -9 $p; done' >& /dev/null
+        ssh $slave ${LOAD_JAVA_COMMAND}; for p in $(${JPS} | grep NodeManager | tr -s " " | cut -d " " -f 1);
+        do echo $p; kill -9 $p; done" >& /dev/null
 done
 
 m_echo "Finishing ResourceManager:" $MASTERNODE
-ssh $MASTERNODE 'for p in $(jps | grep ResourceManager | tr -s " " | cut -d " " -f 1);
-        do echo $p; kill -9 $p; done' >& /dev/null
+ssh $MASTERNODE "${LOAD_JAVA_COMMAND}; for p in $(${JPS} | grep ResourceManager | tr -s " " | cut -d " " -f 1);
+        do echo $p; kill -9 $p; done" >& /dev/null
 
 if [[ $TIMELINE_SERVER == "true" ]]
 then
 	m_echo "Finishing ApplicationHistoryServer:" $MASTERNODE
-	ssh $MASTERNODE 'for p in $(jps | grep ApplicationHistoryServer | tr -s " " | cut -d " " -f 1);
-        	do echo $p; kill -9 $p; done' >& /dev/null
+	ssh $MASTERNODE "${LOAD_JAVA_COMMAND}; for p in $(${JPS} | grep ApplicationHistoryServer | tr -s " " | cut -d " " -f 1);
+        	do echo $p; kill -9 $p; done" >& /dev/null
 fi
 
 if [[ $MR_JOBHISTORY_SERVER == "true" ]]
 then
 	m_echo "Finishing JobHistoryServer:" $MASTERNODE
-	ssh $MASTERNODE 'for p in $(jps | grep JobHistoryServer | tr -s " " | cut -d " " -f 1);
-        	do echo $p; kill -9 $p; done' >& /dev/null
+	ssh $MASTERNODE "${LOAD_JAVA_COMMAND}; for p in $(${JPS} | grep JobHistoryServer | tr -s " " | cut -d " " -f 1);
+        	do echo $p; kill -9 $p; done" >& /dev/null
 fi
 
 sleep 1
