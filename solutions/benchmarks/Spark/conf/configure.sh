1395d75fde0538bc7a11343228b3505324d07f15
Roberto R. Expósito <rreye@udc.es>
2021-02-24 10:57:25 +0100

    Update scala version

--- solutions/benchmarks/Spark/conf/configure.sh
+++ solutions/benchmarks/Spark/conf/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export SORT_PARTITIONS=$(($SPARK_EXECUTORS * $SPARK_CORES_PER_EXECUTOR))
-export SPARK_BENCH_JAR_NAME=sparkbench-2.2_${SPARK_SCALA_VERSION}.jar
+export SPARK_BENCH_JAR_NAME=sparkbench-2.4_${SPARK_SCALA_VERSION}.jar
 
 if [[ $SPARK_SERIES == "1" ]]
 then
@@ -15,14 +15,15 @@ then
 	export SPARK_BENCH_JAR_NAME=sparkbench-3.0_${SPARK_SCALA_VERSION}.jar
 fi
 
-export SPARK_BENCH_JAR=$SOL_BENCH_DIR/bin/$SPARK_BENCH_JAR_NAME
+export SPARK_BENCH_DIR=$SOL_BENCH_DIR/bin/
+export SPARK_BENCH_JAR=$SPARK_BENCH_DIR/$SPARK_BENCH_JAR_NAME
 export SPARK_BENCH_JAR_DELETE="find $SOLUTION_REPORT_DIR -name "$SPARK_BENCH_JAR_NAME" -type f -delete"
 
 if [[ ! -f $SPARK_BENCH_JAR ]]
 then
 	# Download sparkbench jar file
-	URL="http://bdev.des.udc.es/dist/sparkbench"
-	m_echo "Downloading $SPARK_BENCH_JAR_NAME"
+	URL="http://bdev.des.udc.es/dist/sparkbench
+	m_echo "Downloading $SPARK_BENCH_JAR_NAME from $URL"
 
         wget -q -O $SPARK_BENCH_JAR $URL/$SPARK_BENCH_JAR_NAME
 
@@ -30,4 +31,6 @@ then
         then
 		m_exit "Error when downloading $SPARK_BENCH_JAR_NAME"
         fi
+else
+	m_echo "Using $SPARK_BENCH_JAR"
 fi
