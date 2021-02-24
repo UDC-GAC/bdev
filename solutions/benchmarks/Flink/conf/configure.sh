693e9b5f81a30dc226c167a6722773b3f40fe4ea
Roberto R. Exposito <rreye@udc.es>
2021-02-24 11:19:11 +0100

    Fix typo

--- solutions/benchmarks/Flink/conf/configure.sh
+++ solutions/benchmarks/Flink/conf/configure.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-export FLINK_BENCH_JAR_NAME=flinkbench-1.8_${FLINK_SCALA_VERSION}.jar
-export FLINK_BENCH_JAR=$SOL_BENCH_DIR/bin/$FLINK_BENCH_JAR_NAME
+export FLINK_BENCH_JAR_NAME=flinkbench-1.10_${FLINK_SCALA_VERSION}.jar
+export FLINK_BENCH_DIR=$SOL_BENCH_DIR/bin
+export FLINK_BENCH_JAR=$FLINK_BENCH_DIR/$FLINK_BENCH_JAR_NAME
 export SORT_PARTITIONS=$FLINK_PARALLELISM
 
 if [[ ! -f $FLINK_BENCH_JAR ]]
 then
         # Download flinkbench jar file
-        URL="http://bdev.des.udc.es/dist/flinkbench"
+        URL=http://bdev.des.udc.es/dist/flinkbench
         m_echo "Downloading $FLINK_BENCH_JAR_NAME"
 
         wget -q -O $FLINK_BENCH_JAR $URL/$FLINK_BENCH_JAR_NAME
@@ -16,4 +17,6 @@ then
         then
                 m_exit "Error when downloading $FLINK_BENCH_JAR_NAME"
         fi
+else
+	m_echo "Using $FLINK_BENCH_JAR"
 fi
