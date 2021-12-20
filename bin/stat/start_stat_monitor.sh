3975b74c956ae347d36a2a8ffbb2dd9784a6799a
Roberto R. Expósito <rreye@udc.es>
2021-12-20 13:14:23 +0100

    Updates to support dool v1.0.0

--- bin/stat/start_stat_monitor.sh
+++ bin/stat/start_stat_monitor.sh
@@ -3,15 +3,13 @@
 mkdir -p $STATLOGDIR
 
 export NODE_NUMBER=0
+
 for SLAVE in $MASTERNODE $SLAVENODES
 do
 	STATNODEDIR=${STATLOGDIR}/node-${NODE_NUMBER}
+	STATLOGFILE=${STATNODEDIR}/stat.csv
 	mkdir -p ${STATNODEDIR}
-	echo "Starting dstat monitor in ${SLAVE}, storing data on ${STATNODEDIR}" >> ${STATLOGDIR}/log 2>&1
-	nohup ssh $SLAVE "export STATLOGFILE=${STATNODEDIR}/stat.csv; \
-		export DSTAT=${DSTAT}; \
-		export STAT_SECONDS_INTERVAL=${STAT_SECONDS_INTERVAL}; \
-		bash ${STAT_HOME}/stat_monitor.sh" > ${STATNODEDIR}/stat.out 2>&1 &
-	
+	echo "Starting dstat/dool monitor in ${SLAVE}, storing data on ${STATNODEDIR}" >> ${STATLOGDIR}/log 2>&1
+	nohup ssh $SLAVE "${PYTHON3_BIN} ${DOOL_COMMAND} ${DOOL_OPTIONS} --output ${STATLOGFILE} ${STAT_SECONDS_INTERVAL}" > ${STATNODEDIR}/stat.out 2>&1 &
 	export NODE_NUMBER=$(( $NODE_NUMBER + 1 ))
 done
