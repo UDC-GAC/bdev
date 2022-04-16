5136a7dd461971577b5a3b1ce2ebc5d35a3c8526
TomeMD <tome.maseda@udc.es>
2022-04-16 17:35:44 +0200

    Add support for TPCx-HS optimized sort with Spark

--- solutions/benchmarks/Spark/bin/tpcx_hs/run.sh
+++ solutions/benchmarks/Spark/bin/tpcx_hs/run.sh
@@ -3,7 +3,7 @@
 $HADOOP_EXECUTABLE fs ${RMR} $OUTPUT_TPCX_HS
 
 run_benchmark "$SPARK_HOME/bin/spark-submit \
-    --class es.udc.tpcx_hs.spark.HSSort ${DEPLOY_ARGS} $TPCX_HS_JAR \
+    --class es.udc.tpcx_hs.spark.${SPARK_HSSORT_IMPL} ${DEPLOY_ARGS} $TPCX_HS_JAR \
     $INPUT_TPCX_HS $OUTPUT_TPCX_HS"
 
 if [ $(cat $TMPLOGFILE | grep -i -E "final status: FAILED" | wc -l) != "0" ]
