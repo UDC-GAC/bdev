e71be59dd1940941d653bdb886430b66b69a852a
Roberto R. Expósito <rreye@udc.es>
2022-04-21 11:15:24 +0200

    Add support for setting HDFS write packet size

--- etc/hdfs-default.sh
+++ etc/hdfs-default.sh
@@ -6,6 +6,7 @@ export NAMENODE_D_HEAPSIZE=1024		# NameNode daemon heapsize (MB)
 export DATANODE_D_HEAPSIZE=1024		# DataNode daemon heapsize (MB)
 export BLOCKSIZE=$((128*1024*1024)) 	# HDFS block size (Bytes)
 export REPLICATION_FACTOR=3		# Number of block replications
+export CLIENT_WRITE_PACKET_SIZE=131072	# Packet size for clients to write
 export NAMENODE_HANDLER_COUNT=50	# Number of NameNode RPC server threads: ln(#DataNodes)*20 (eg, 50 threads for 12 DataNodes)
 export NAMENODE_ACCESTIME_PRECISION=0	# Last access time (milliseconds) for HDFS files is precise upto this value (0 turns off access times)
 export SHORT_CIRCUIT_LOCAL_READS=false	# Enable short-circuit local reads to bypass the DataNode, allowing the client to read the file directly
