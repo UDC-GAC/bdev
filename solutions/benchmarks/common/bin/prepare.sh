5e2404bf00c349b924b489ca6e2fa803391d3097
TomeMD <tome.maseda@udc.es>
2022-04-28 17:15:46 +0200

    Add full path to TPCx-HS Hadoop classes

--- solutions/benchmarks/common/bin/prepare.sh
+++ solutions/benchmarks/common/bin/prepare.sh
@@ -194,7 +194,7 @@ if [[ $GEN_TPCX_HS == "true" ]]
 then
 	m_echo "Generating TPCx-HS data: ${TPCX_HS_DATASIZE} bytes"
 
-	OPTIONS="HSGen \
+	OPTIONS="es.udc.tpcx_hs.datagen.HSGen \
     -D $CONFIG_MAP_NUMBER=${MAPPERS_NUMBER} \
     -D $CONFIG_REDUCER_NUMBER=${REDUCERS_NUMBER} \
     $TPCX_HS_DATASIZE $INPUT_TPCX_HS"
