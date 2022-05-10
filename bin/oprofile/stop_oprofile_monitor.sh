477d99a3d60357c0fbb76ab5c63f0dec858f4fd3
Roberto R. Exposito <rreye@udc.es>
2022-05-10 09:04:07 +0200

    Improve cleanup

--- bin/oprofile/stop_oprofile_monitor.sh
+++ bin/oprofile/stop_oprofile_monitor.sh
@@ -2,5 +2,5 @@
 
 for SLAVE in $SLAVENODES $MASTERNODE
 do
-	ssh $SLAVE "killall -SIGINT ocount"
+	ssh $SLAVE "killall -u $USER -q -SIGINT ocount"
 done
--- solutions/src/RDMA-Spark/bin/finish.sh
+++ solutions/src/RDMA-Spark/bin/finish.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+$SPARK_HOME/sbin/stop-all.sh
+
 bash $CLEAN_DAEMONS_SCRIPT
 
 $COMMON_SRC_DIR/bin/spark-config.sh
