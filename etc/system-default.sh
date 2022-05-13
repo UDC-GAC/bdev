1fbe623a9e67ac1c9049ddd548dff96c943a6c5b
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2022-05-13 17:01:28 +0200

    Add variable to set IP for loopback interface

--- etc/system-default.sh
+++ etc/system-default.sh
@@ -4,6 +4,7 @@
 
 export TMP_DIR=/tmp/$USER/$METHOD_NAME	# Directory to store temporary files
 export LOCAL_DIRS="" 		# Comma-separated list of directories to store local data in each node
+export LOOPBACK_IP=127.0.0.1	# IP of the loopback network interface
 export ETH_INTERFACE=eth0 	# Ethernet interface to use in the nodes
 export IPOIB_INTERFACE=ib0 	# IPoIB interface to use in the nodes
 export CPUS_PER_NODE=`grep "^physical id" /proc/cpuinfo | sort -u | wc -l`	# CPUs per node
