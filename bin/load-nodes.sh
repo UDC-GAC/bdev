d1cb7f066aa2eae703d7d995a3569e748717a3e0
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2022-05-13 14:13:56 +0200

    Improve error handling when resolving hostnames and IPs

--- bin/load-nodes.sh
+++ bin/load-nodes.sh
@@ -13,19 +13,26 @@ export RESOLVEIP_COMMAND=`which getent`
 
 export NODE_FILE=$REPORT_DIR/hostfile
 export COMPUTE_NODES=`get_nodes_by_hostname $NODE_FILE $COMPUTE_NODES`
+if [[ -z "${COMPUTE_NODES}" ]]; then
+	m_exit "Nodes: Revise network settings"
+fi
 m_echo "Nodes: "$COMPUTE_NODES
 
-if [[ ! -z $ETH_INTERFACE ]]
-then
+if [[ ! -z $ETH_INTERFACE ]]; then
 	export NODE_FILE_ETH=$REPORT_DIR/hostfile.eth
 	export ETH_COMPUTE_NODES=`get_nodes_by_interface $NODE_FILE_ETH $ETH_INTERFACE $COMPUTE_NODES`
+	if [[ -z "${ETH_COMPUTE_NODES}" ]]; then
+		m_exit "ETH ($ETH_INTERFACE): Revise network settings"
+	fi
 	m_echo "ETH ($ETH_INTERFACE): "$ETH_COMPUTE_NODES
 fi
 
-if [[ ! -z $IPOIB_INTERFACE ]]
-then
+if [[ ! -z $IPOIB_INTERFACE ]]; then
 	export NODE_FILE_IPOIB=$REPORT_DIR/hostfile.ipoib
        	export IPOIB_COMPUTE_NODES=`get_nodes_by_interface $NODE_FILE_IPOIB $IPOIB_INTERFACE $COMPUTE_NODES`
+	if [[ -z "${IPOIB_COMPUTE_NODES}" ]]; then
+		m_exit "IPoIB ($IPOIB_INTERFACE): Revise network settings"
+	fi
 	m_echo "IPoIB ($IPOIB_INTERFACE): "$IPOIB_COMPUTE_NODES
 fi
 
