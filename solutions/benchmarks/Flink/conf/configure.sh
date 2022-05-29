dc67dff53b6043ec1a0912b5f58cbda21c837a89
Tomé Maseda Dorado <tome.maseda@udc.es>
2022-05-29 12:18:28 +0200

    Update configure.sh
    Unnecessary JAR download removed

--- solutions/benchmarks/Flink/conf/configure.sh
+++ solutions/benchmarks/Flink/conf/configure.sh
@@ -5,7 +5,11 @@ export FLINK_BENCH_DIR=$SOL_BENCH_DIR/bin
 export FLINK_BENCH_JAR=$FLINK_BENCH_DIR/$FLINK_BENCH_JAR_NAME
 export SORT_PARTITIONS=$FLINK_PARALLELISM
 
-if [[ ! -f $FLINK_BENCH_JAR ]]
+
+if [[ "$BENCHMARK" == "tpcx_hs" ]]
+then
+	m_echo "Using $TPCX_HS_JAR"
+elif [[ ! -f $FLINK_BENCH_JAR ]]
 then
         # Download flinkbench jar file
         URL=http://bdev.des.udc.es/dist/flinkbench
@@ -18,9 +22,6 @@ then
 		rm $FLINK_BENCH_JAR >& /dev/null
                 m_exit "Error when downloading $FLINK_BENCH_JAR_NAME"
         fi
-elif [[ "$BENCHMARK" == "tpcx_hs" ]]
-then
-	m_echo "Using $TPCX_HS_JAR"
 else
 	m_echo "Using $FLINK_BENCH_JAR"
 fi
