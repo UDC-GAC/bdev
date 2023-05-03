eb872f2688665f1ad6287038b92b1a516c057885
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2023-05-03 13:15:02 +0200

    Update supported versions for Flink

--- solutions/src/Flink-YARN/etc/env.sh
+++ solutions/src/Flink-YARN/etc/env.sh
@@ -67,7 +67,7 @@ export DEPLOY_ARGS="-m yarn-cluster \
 
 if [[ $FLINK_SERIES == "1" ]]
 then
-	if [[ $FLINK_MAJOR_VERSION != "1.15" ]] && [[ $FLINK_MAJOR_VERSION != "1.14" ]] && [[ $FLINK_MAJOR_VERSION != "1.13" ]] && [[ $FLINK_MAJOR_VERSION != "1.12" ]]
+        if [[ $FLINK_MAJOR_VERSION != "1.17" ]] && [[ $FLINK_MAJOR_VERSION != "1.16" ]] && [[ $FLINK_MAJOR_VERSION != "1.15" ]] && [[ $FLINK_MAJOR_VERSION != "1.14" ]] && [[ $FLINK_MAJOR_VERSION != "1.13" ]]
 	then
 		m_exit "Flink version is not supported: $FLINK_MAJOR_VERSION"
 	fi
