954c1b387dda67f8c582d46a8e570ee651e71b13
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-06 23:52:15 +0200

    Remove memory configuration from gen-config.sh
    Removed memory configuration lines for JobManager and TaskManagers from gen-config.sh.

--- solutions/src/Flink-YARN/bin/gen-config.sh
+++ solutions/src/Flink-YARN/bin/gen-config.sh
@@ -2,10 +2,6 @@
 m_echo "Flink configuration"
 bash $OLD_GEN_CONFIG_SCRIPT
 
-# Add here memory configuration for JobManager and Taskmanagers
-echo "${FLINK_JOBMANAGER_MEMORY_PARAM}m" >> $FLINK_CONFIG_YAML_FILE
-echo "${FLINK_TASKMANAGER_MEMORY_PARAM}m" >> $FLINK_CONFIG_YAML_FILE
-
 export SOL_TEMPLATE_DIR=$HADOOP_TEMPLATE_DIR
 export SOL_CONF_DIR_SRC=$HADOOP_CONF_DIR_SRC
 export SOL_CONF_DIR=$HADOOP_CONF_DIR
