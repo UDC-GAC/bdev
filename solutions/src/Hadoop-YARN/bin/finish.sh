ee759ec09e8ac5719cab7f879b1556d6f3108b84
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 01:13:47 +0200

    Update finish.sh to include additional cleanup scripts

--- solutions/src/Hadoop-YARN/bin/finish.sh
+++ solutions/src/Hadoop-YARN/bin/finish.sh
@@ -2,5 +2,7 @@
 
 export FORCE_DELETE_HDFS=false
 bash $YARN_KILLALL_SCRIPT
+$COMMON_SRC_DIR/bin/finish_hdfs.sh
+$COMMON_SRC_DIR/bin/finish_yarn.sh
 bash $CLEAN_DAEMONS_SCRIPT
 bash $CLEAN_DATA_SCRIPT
