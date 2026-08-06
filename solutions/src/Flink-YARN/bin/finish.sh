43a725d6033a306a50d6fd406f6f6dd1082de71c
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 01:18:41 +0200

    Simplify finish.sh by removing unnecessary scripts
    Removed redundant cleanup scripts and streamlined the finish process.

--- solutions/src/Flink-YARN/bin/finish.sh
+++ solutions/src/Flink-YARN/bin/finish.sh
@@ -2,7 +2,4 @@
 
 echo "stop" | $FLINK_HOME/bin/yarn-session.sh -id $YARN_APP_ID
 
-bash $YARN_KILLALL_SCRIPT
-${COMMON_SRC_DIR}/bin/finish_yarn.sh
-${COMMON_SRC_DIR}/bin/finish_hdfs.sh
-bash $CLEAN_DAEMONS_SCRIPT
+$COMMON_SRC_DIR/bin/stop_hadoop_yarn.sh
