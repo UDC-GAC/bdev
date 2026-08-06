5aa50e03d657f0c74c138dda1151208ca0ce3a09
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 00:42:14 +0200

    Update finish.sh to include additional cleanup scripts

--- solutions/src/Flink-YARN/bin/finish.sh
+++ solutions/src/Flink-YARN/bin/finish.sh
@@ -3,4 +3,6 @@
 echo "stop" | $FLINK_HOME/bin/yarn-session.sh -id $YARN_APP_ID
 
 bash $YARN_KILLALL_SCRIPT
+${COMMON_SRC_DIR}/bin/finish_yarn.sh
+${COMMON_SRC_DIR}/bin/finish_hdfs.sh
 bash $CLEAN_DAEMONS_SCRIPT
