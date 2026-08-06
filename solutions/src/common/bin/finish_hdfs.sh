f6341b5cb11cf57c156f390b91d7c3583f118b51
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 00:34:08 +0200

    Update finish_hdfs.sh

--- solutions/src/common/bin/finish_hdfs.sh
+++ solutions/src/common/bin/finish_hdfs.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-SLAVES=`cat $SLAVESFILE`
-for slave in $SLAVES
-do
-	m_echo "Finishing DataNode:" $slave
-        $SSH_CMD $slave "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS DataNode"
-done
-
-m_echo "Finishing NameNode:" $MASTERNODE
-$SSH_CMD $MASTERNODE "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS NameNode"
-
-sleep 1
+${HADOOP_HOME}/sbin/stop-dfs.sh" --config "${HADOOP_CONF_DIR}"
