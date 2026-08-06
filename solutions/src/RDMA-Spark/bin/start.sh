d4aa5cd80fed6a0200ab6bb423b5d733d89ec04f
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 01:24:21 +0200

    Optimize Spark cluster startup script
    Reduced sleep duration before starting slaves and removed unnecessary Spark configuration.

--- solutions/src/RDMA-Spark/bin/start.sh
+++ solutions/src/RDMA-Spark/bin/start.sh
@@ -2,12 +2,10 @@
 
 $COMMON_SRC_DIR/bin/start_hadoop_yarn.sh
 
-#Spark
-$COMMON_SRC_DIR/bin/spark-config.sh set
-
+m_echo "Starting the standalone Spark cluster"
 $SPARK_HOME/sbin/start-master.sh &
 
-sleep 10
+sleep 5
 
 $SPARK_HOME/sbin/start-slaves.sh &
 
