7b43c2d98885e459782360ba9017092eec9b170d
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 01:03:22 +0200

    Check for stop-cluster.sh before stopping cluster
    Add check for existence of stop-cluster.sh before execution.

--- solutions/src/Flink/bin/finish.sh
+++ solutions/src/Flink/bin/finish.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-bash $FLINK_HOME/bin/stop-cluster.sh
+# Avoid cleanup if solution does not exist
+SCRIPT=$FLINK_HOME/bin/stop-cluster.sh
+
+if [ -f "$SCRIPT" ]; then
+	bash $SCRIPT
+fi
+
 bash $CLEAN_DAEMONS_SCRIPT
