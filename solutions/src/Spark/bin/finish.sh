bc725c459ce8b2266e03c2121e897c6ef4fe93a1
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 01:02:37 +0200

    Check for existence of stop-all.sh before execution
    Added a check to ensure the stop-all.sh script exists before executing it.

--- solutions/src/Spark/bin/finish.sh
+++ solutions/src/Spark/bin/finish.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-bash $SPARK_HOME/sbin/stop-all.sh
+# Avoid cleanup if solution does not exist
+SCRIPT=$SPARK_HOME/sbin/stop-all.sh
+
+if [ -f "$SCRIPT" ]; then
+	bash $SCRIPT
+fi
+
 bash $CLEAN_DAEMONS_SCRIPT
 
 # Get rid of jar files in user logs
