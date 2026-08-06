d1bfdf4fba71338a4ce2b527715a140cc6161b92
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 01:19:32 +0200

    Remove unnecessary cleanup scripts from finish.sh

--- solutions/src/Hadoop-YARN/bin/finish.sh
+++ solutions/src/Hadoop-YARN/bin/finish.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-export FORCE_DELETE_HDFS=false
-bash $YARN_KILLALL_SCRIPT
-$COMMON_SRC_DIR/bin/finish_hdfs.sh
-$COMMON_SRC_DIR/bin/finish_yarn.sh
-bash $CLEAN_DAEMONS_SCRIPT
-bash $CLEAN_DATA_SCRIPT
+$COMMON_SRC_DIR/bin/stop_hadoop_yarn.sh
