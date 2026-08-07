946c6a14df2fca14ea4b25d77ddfa97c54da2096
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 12:44:58 +0200

    Simplify run.sh by removing legacy Hadoop checks
    Removed conditional checks and commands for Hadoop version 1.

--- solutions/benchmarks/Hadoop/bin/aggregation/run.sh
+++ solutions/benchmarks/Hadoop/bin/aggregation/run.sh
@@ -2,14 +2,6 @@
 
 ${HDFS_CMD} ${RMR} $OUTPUT_AGGREGATION
 
-if [[ "x$HADOOP_MR_VERSION" != "xYARN" ]]
-then
-	# These lines are needed for Hadoop version 1
-        ${HDFS_CMD} ${MKDIR} ${HIVE_TMP_DIR}
-        # The root scratch dir: hdfs://XXXX on HDFS should be writable
-        ${HDFS_CMD} ${CHMOD} 733 ${HIVE_TMP_DIR}
-fi
-
 HIVE_SQL_FILE=${BENCHMARK_OUTPUT_DIR}/uservisits_aggregation.hive
 prepare_sql_aggregation ${HIVE_SQL_FILE}
 
