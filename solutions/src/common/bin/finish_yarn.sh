a1f104cd66e5b871d1ff598837658515bc4540f0
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 10:52:40 +0200

    Enhance finish_yarn.sh with forced shutdown options

--- solutions/src/common/bin/finish_yarn.sh
+++ solutions/src/common/bin/finish_yarn.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-"${HADOOP_HOME}/sbin/stop-yarn.sh" --config "${HADOOP_CONF_DIR}"
+if [[ "x${FINISH_YARN_FORCE}" == "xtrue" ]]
+  SLAVES=`cat $SLAVESFILE`
+  for slave in $SLAVES
+  do
+	  m_echo "Finishing NodeManager:" $slave
+    $SSH_CMD $slave "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS NodeManager"
+  done
+
+  m_echo "Finishing ResourceManager:" $MASTERNODE
+  $SSH_CMD $MASTERNODE "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS ResourceManager"
+
+  if [[ $TIMELINE_SERVER == "true" ]]
+  then
+	  m_echo "Finishing ApplicationHistoryServer:" $MASTERNODE
+	  $SSH_CMD $MASTERNODE "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS ApplicationHistoryServer"
+  fi
+
+  if [[ $MR_JOBHISTORY_SERVER == "true" ]]
+  then
+	  m_echo "Finishing JobHistoryServer:" $MASTERNODE
+	  $SSH_CMD $MASTERNODE "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS JobHistoryServer"
+  fi
+else
+  "${HADOOP_HOME}/sbin/stop-yarn.sh" --config "${HADOOP_CONF_DIR}"
+fi
