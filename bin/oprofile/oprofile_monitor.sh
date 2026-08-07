d35f8521b8152df4df95c139fc23abbb2759c412
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 15:58:07 +0200

    Fix conditional syntax in oprofile_monitor.sh

--- bin/oprofile/oprofile_monitor.sh
+++ bin/oprofile/oprofile_monitor.sh
@@ -2,11 +2,10 @@
 
 OCOUNT_ARGS="-s -b -f ${OPROFILELOGFILE}"
 
-if [[ ${OPROFILE_EVENTS} != "" ]]
+if [ ${OPROFILE_EVENTS} != "" ]
 then
 	echo "Monitoring events: ${OPROFILE_EVENTS}"
 	OCOUNT_ARGS="$OCOUNT_ARGS -e ${OPROFILE_EVENTS}"
 fi
 
 ${OPROFILE_BIN_DIR}/ocount $OCOUNT_ARGS
-
