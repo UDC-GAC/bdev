3cc2fb4eb3357d08fbabfe21e3e3e0f2f42ccae5
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 11:45:42 +0200

    Refactor Spark start script to improve execution flow
    Removed sleep commands and adjusted script flow for starting Spark components.

--- solutions/src/Spark/bin/start.sh
+++ solutions/src/Spark/bin/start.sh
@@ -3,19 +3,14 @@
 $COMMON_SRC_DIR/bin/start_hadoop_yarn.sh
 
 m_echo "Starting the standalone Spark cluster"
-$SPARK_HOME/sbin/start-master.sh &
-
-sleep 5
-
-$SPARK_HOME/sbin/start-slaves.sh &
+$SPARK_HOME/sbin/start-master.sh
+$SPARK_WORKERS_START_SCRIPT
 
 if [[ $SPARK_HISTORY_SERVER == "true" ]]
 then
 	${HDFS_CMD} ${MKDIR} ${SPARK_HISTORY_SERVER_DIR}
 	${HDFS_CMD} ${CHMOD} 777 ${SPARK_HISTORY_SERVER_DIR}
 
-        #Spark history server
-	$SPARK_HOME/sbin/start-history-server.sh &
+    #Spark history server
+	$SPARK_HOME/sbin/start-history-server.sh
 fi
-
-sleep 10
