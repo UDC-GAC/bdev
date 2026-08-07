0a199f7cbe3e60cff321624f9e4fab7ccf269a9c
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 15:11:46 +0200

    Create env.sh for Hadoop YARN environment variables
    Add YARN environment variable configurations for Hadoop.

---
+++ solutions/src/common/env.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#YARN environment variables
+export HADOOP_HOME=$SPARK_HADOOP_HOME
+export HADOOP_CONF_DIR_SRC=$HADOOP_HOME/etc/hadoop
+export HADOOP_CONF_DIR=$SOLUTION_REPORT_DIR/conf/hadoop
+export HADOOP_LOG_DIR=$SOLUTION_REPORT_DIR/logs/hadoop
+export YARN_CONF_DIR=$HADOOP_CONF_DIR
+export YARN_LOG_DIR=$SOLUTION_REPORT_DIR/logs/hadoop
+export PATH=$HADOOP_HOME/bin:$PATH
+export HADOOP_VERSION=${HADOOP_HOME##*/}
+export HADOOP_MAJOR_VERSION=${HADOOP_VERSION%.*}
+export HADOOP_SERIES=${HADOOP_VERSION%%.*}
+
+if [[ $HADOOP_SERIES == "3" ]]; then
+	export HADOOP_TEMPLATE_DIR=$TEMPLATES_DIR/Hadoop-YARN-3
+	export HADOOP_DAEMONS_DIR=$DAEMONS_DIR/Hadoop-YARN-3
+	export HADOOP_SBIN_DIR=$SOLUTION_HOME/libexec
+	export HADOOP_SLAVESFILE=$SOL_CONF_DIR/workers
+elif [[ $HADOOP_SERIES == "2" ]]; then
+	export HADOOP_TEMPLATE_DIR=$TEMPLATES_DIR/Hadoop-YARN
+	export HADOOP_DAEMONS_DIR=$DAEMONS_DIR/Hadoop-YARN
+	export HADOOP_SBIN_DIR=$SOLUTION_HOME/sbin
+	export HADOOP_SLAVESFILE=$SOL_CONF_DIR/slaves
+else
+	m_exit "Hadoop version is not supported: $HADOOP_VERSION"
+fi
