5292d58a2006b8d5d62f9468460b8e5958db169b
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-08 01:00:50 +0200

    Refactor finish_yarn.sh for consistent formatting

--- solutions/src/common/bin/finish_yarn.sh
+++ solutions/src/common/bin/finish_yarn.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 if [[ -v FINISH_YARN_FORCE && "$FINISH_YARN_FORCE" == "true" ]]; then
-  SLAVES=`cat $SLAVESFILE`
-  for slave in $SLAVES
-  do
-	  m_echo "Finishing NodeManager:" $slave
-    $SSH_CMD $slave "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS NodeManager"
-  done
+	SLAVES=`cat $SLAVESFILE`
+	for slave in $SLAVES
+	do
+		m_echo "Finishing NodeManager:" $slave
+		$SSH_CMD $slave "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS NodeManager"
+	done
 
-  m_echo "Finishing ResourceManager:" $MASTERNODE
-  $SSH_CMD $MASTERNODE "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS ResourceManager"
+  	m_echo "Finishing ResourceManager:" $MASTERNODE
+	$SSH_CMD $MASTERNODE "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS ResourceManager"
 
-  if [[ $TIMELINE_SERVER == "true" ]]
-  then
-	  m_echo "Finishing ApplicationHistoryServer:" $MASTERNODE
-	  $SSH_CMD $MASTERNODE "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS ApplicationHistoryServer"
-  fi
+  	if [[ $TIMELINE_SERVER == "true" ]]; then
+		m_echo "Finishing ApplicationHistoryServer:" $MASTERNODE
+		$SSH_CMD $MASTERNODE "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS ApplicationHistoryServer"
+	fi
 
-  if [[ $MR_JOBHISTORY_SERVER == "true" ]]
-  then
-	  m_echo "Finishing JobHistoryServer:" $MASTERNODE
-	  $SSH_CMD $MASTERNODE "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS JobHistoryServer"
-  fi
+  	if [[ $MR_JOBHISTORY_SERVER == "true" ]]; then
+		m_echo "Finishing JobHistoryServer:" $MASTERNODE
+		$SSH_CMD $MASTERNODE "${LOAD_JAVA_COMMAND}; ${METHOD_BIN_DIR}/kill.sh $JPS JobHistoryServer"
+  	fi
 else
-  "${HADOOP_HOME}/sbin/stop-yarn.sh" --config "${HADOOP_CONF_DIR}"
+	m_echo "Stopping YARN services"
+	"${HADOOP_HOME}/sbin/stop-yarn.sh" --config "${HADOOP_CONF_DIR}"
 fi
