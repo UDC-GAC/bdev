04ce190b94a7c9edb852e44317160af7dcbad78b
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 11:45:05 +0200

    Rename WORKERS_SCRIPT to SPARK_WORKERS_SCRIPT

--- solutions/src/Spark/etc/env.sh
+++ solutions/src/Spark/etc/env.sh
@@ -70,9 +70,9 @@ export DEPLOY_ARGS="--master spark://${MASTERNODE}:7077 --deploy-mode client"
 if [[ $SPARK_SERIES == "0" ]] || [[ $SPARK_SERIES == "1" ]]
 	m_exit "Spark version is not supported: $SPARK_MAJOR_VERSION"
 else if [[ $SPARK_SERIES == "2" ]]
-	WORKERS_SCRIPT=$SPARK_HOME/sbin/start-slaves.sh
+	SPARK_WORKERS_SCRIPT=$SPARK_HOME/sbin/start-slaves.sh
 else
-	WORKERS_SCRIPT=$SPARK_HOME/sbin/start-workers.sh
+	SPARK_WORKERS_SCRIPT=$SPARK_HOME/sbin/start-workers.sh
 fi
 
 add_conf_param "spark_conf_dir" $SPARK_CONF_DIR
