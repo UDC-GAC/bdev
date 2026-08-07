2c5b03e2d246c93a3e32c602e0708f974f9f0058
Roberto R. Exposito <rreye@udc.es>
2026-08-08 00:48:29 +0200

    Fix bug

--- solutions/templates/Hadoop-YARN/yarn-env.sh
+++ solutions/templates/Hadoop-YARN/yarn-env.sh
@@ -111,7 +111,16 @@ unset IFS
 
 export YARNHOSTNAME=`$method_bin_dir/$hostname_script $hostfile $loopback_ip`
 export YARN_OPTS="-Djava.net.preferIPv4Stack=true -Djava.io.tmpdir=$tmp_dir -DYARNHOSTNAME=${YARNHOSTNAME}"
-export YARN_OPTS="$YARN_OPTS --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED"
+
+JAVA_BIN="${JAVA_HOME:+$JAVA_HOME/bin/}java"
+JAVA_VER_STR=$("$JAVA_BIN" -version 2>&1 | head -n 1 | cut -d '"' -f 2)
+JAVA_MAJOR_VER=$(echo "$JAVA_VER_STR" | awk -F '.' '{print ($1 == "1") ? $2 : $1}')
+
+if [ -n "$JAVA_MAJOR_VER" ] && [ "$JAVA_MAJOR_VER" -ge 9 ] 2>/dev/null; then
+    _ADD_OPENS="--add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED"
+
+    export YARN_OPTS="$YARN_OPTS $_ADD_OPENS"
+fi
 
 YARN_OPTS="$YARN_OPTS -Dhadoop.log.dir=$YARN_LOG_DIR"
 YARN_OPTS="$YARN_OPTS -Dyarn.log.dir=$YARN_LOG_DIR"
