2a42186cf6f46450f7094efc54f541024468c0b0
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 13:04:08 +0200

    Simplify daemon copying logic in copy-daemons.sh
    Refactored the script to simplify daemon copying logic and removed unnecessary checks for SGE and STD daemons.

--- bin/copy-daemons.sh
+++ bin/copy-daemons.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
 
-if [[ "$SGE_ENV" == "true" ]]
+if [[ ! -z $SOL_DAEMONS_DIR ]]
 then
-	if [[ ! -z $SOL_SGE_DAEMONS_DIR ]]
-	then
-		m_echo "Copying daemons from $SOL_SGE_DAEMONS_DIR"
-		cp $SOL_SGE_DAEMONS_DIR/* $SOL_SBIN_DIR 2> /dev/null
-	else
-		m_echo "No daemons directory to copy"
-	fi
+	m_echo "Copying daemons from $SOL_DAEMONS_DIR"
+	cp $SOL_DAEMONS_DIR/* $SOL_SBIN_DIR 2> /dev/null
 else
-	if [[ ! -z $SOL_STD_DAEMONS_DIR ]]
-	then
-		m_echo "Copying daemons from $SOL_STD_DAEMONS_DIR"
-		cp $SOL_STD_DAEMONS_DIR/* $SOL_SBIN_DIR 2> /dev/null
-	else
-		m_echo "No daemons directory to copy"
-	fi
+	m_echo "No daemons directory to copy"
 fi
-
