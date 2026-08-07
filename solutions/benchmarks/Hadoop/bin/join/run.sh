075cb1acfa77be427336215d3747b8d3583c7b86
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 12:45:16 +0200

    Clean up run.sh by removing version check for YARN
    Removed obsolete Hadoop version check and related commands.

--- solutions/benchmarks/Hadoop/bin/join/run.sh
+++ solutions/benchmarks/Hadoop/bin/join/run.sh
@@ -2,14 +2,6 @@
 
 ${HDFS_CMD} ${RMR} $OUTPUT_JOIN
 
-if [[ "x$HADOOP_MR_VERSION" != "xYARN" ]]
-then
-        # These lines are needed for Hadoop version 1
-        ${HDFS_CMD} ${MKDIR} ${HIVE_TMP_DIR}
-	# The root scratch dir: hdfs://XXXX on HDFS should be writable
-        ${HDFS_CMD} ${CHMOD} 733 ${HIVE_TMP_DIR}
-fi
-
 HIVE_SQL_FILE=${BENCHMARK_OUTPUT_DIR}/rankings_uservisits_join.hive
 prepare_sql_join ${HIVE_SQL_FILE}
 
