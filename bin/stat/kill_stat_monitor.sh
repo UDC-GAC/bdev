73c4680ea82e7276f61dac9a41fb4c4bbe21511c
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 15:56:40 +0200

    Fix conditional syntax in kill_stat_monitor.sh

--- bin/stat/kill_stat_monitor.sh
+++ bin/stat/kill_stat_monitor.sh
@@ -2,7 +2,7 @@
 
 DOOL_PID=`ps -elf | grep ${PYTHON3_BIN} | grep ${DOOL_COMMAND_NAME} | grep -v "ssh" | grep -v "export" | awk '{print $4}'`
 
-if [[ \"x$DOOL_PID\" != \"x\" ]]; then
+if [ \"x$DOOL_PID\" != \"x\" ]; then
 	DOOL_PID=`echo $DOOL_PID`
 	echo "$HOSTNAME: cleaning up ${DOOL_COMMAND_NAME} with PID $DOOL_PID"
 	kill -9 $DOOL_PID
