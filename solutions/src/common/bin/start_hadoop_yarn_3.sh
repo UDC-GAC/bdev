d1bde7e98f9a972e620db1cab21deb3a5ed7bd5c
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 22:30:03 +0200

    Refactor DataNode and NodeManager startup process
    Removed individual DataNode and NodeManager start loops in favor of starting all at once.

--- solutions/src/common/bin/start_hadoop_yarn_3.sh
+++ solutions/src/common/bin/start_hadoop_yarn_3.sh
@@ -7,32 +7,26 @@ if [[ $FORCE_FORMAT_HDFS == "true" ]]; then
 	$SSH_CMD $MASTERNODE "$HDFS_CONFIG $HADOOP_CONF_DIR namenode -format -force -clusterID CID-bdev" > $HDFS_FORMAT_LOG 2>&1
 fi
 
-SLAVES=`cat $SLAVESFILE`
-
 #Namenode & Datanodes
 m_echo "Starting NameNode:" $MASTERNODE
 $SSH_CMD $MASTERNODE "$HDFS_CONFIG $HADOOP_CONF_DIR --daemon start namenode"
-for slave in $SLAVES
-do
-	m_echo "Starting DataNode:" $slave
-	$SSH_CMD $slave "$HDFS_CONFIG $HADOOP_CONF_DIR --daemon start datanode"
-done
 
 if [[ $SECONDARY_NAMENODE == "true" ]]
 then
 	#Secondary NameNode
+	m_echo "Starting Secondary NameNode:" $MASTERNODE
 	$SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR start secondarynamenode"
 fi
+
+m_echo "Starting DataNodes"
+$HADOOP_HOME/sbin/hadoop-daemons.sh --config $HADOOP_CONF_DIR --script hdfs start datanode
 
 #Resourcemanager & Nodemanagers
 m_echo "Starting ResourceManager:" $MASTERNODE
 $SSH_CMD $MASTERNODE "$YARN_CONFIG $HADOOP_CONF_DIR --daemon start resourcemanager"
 
-for slave in $SLAVES
-do
-	m_echo "Starting NodeManager:" $slave
-	$SSH_CMD $slave "$YARN_CONFIG $HADOOP_CONF_DIR --daemon start nodemanager"
-done
+m_echo "Starting NodeManagers"
+$HADOOP_HOME/sbin/yarn-daemons.sh --config $HADOOP_CONF_DIR start nodemanager
 
 if [[ $TIMELINE_SERVER == "true" ]]
 then
