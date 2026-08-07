115dceef0b894a88d495c92c97accbec1fb188e8
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 16:01:58 +0200

    Update start.sh

--- solutions/src/Spark-YARN/bin/start.sh
+++ solutions/src/Spark-YARN/bin/start.sh
@@ -2,7 +2,7 @@
 
 $COMMON_SRC_DIR/bin/start_hadoop_yarn.sh
 
-if [[ $SPARK_HISTORY_SERVER == "true" ]]
+if [ $SPARK_HISTORY_SERVER == "true" ]
 then
         ${HDFS_CMD} ${MKDIR} ${SPARK_HISTORY_SERVER_DIR}
         ${HDFS_CMD} ${CHMOD} 777 ${SPARK_HISTORY_SERVER_DIR}
