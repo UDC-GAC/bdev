c71d9b8b772644cd97dffeec5b57968ca64623b4
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 11:40:48 +0200

    Fix script syntax and formatting issues

--- solutions/src/common/bin/start_hadoop_yarn_3.sh
+++ solutions/src/common/bin/start_hadoop_yarn_3.sh
@@ -11,33 +11,33 @@ SLAVES=`cat $SLAVESFILE`
 
 #Namenode & Datanodes
 m_echo "Starting NameNode:" $MASTERNODE
-$SSH_CMD $MASTERNODE "$HDFS_CONFIG $HADOOP_CONF_DIR --daemon start namenode" &
+$SSH_CMD $MASTERNODE "$HDFS_CONFIG $HADOOP_CONF_DIR --daemon start namenode"
 for slave in $SLAVES
 do
 	m_echo "Starting DataNode:" $slave
-	$SSH_CMD $slave "$HDFS_CONFIG $HADOOP_CONF_DIR --daemon start datanode" &
+	$SSH_CMD $slave "$HDFS_CONFIG $HADOOP_CONF_DIR --daemon start datanode"
 done
 
 #Resourcemanager & Nodemanagers
 m_echo "Starting ResourceManager:" $MASTERNODE
-$SSH_CMD $MASTERNODE "$YARN_CONFIG $HADOOP_CONF_DIR --daemon start resourcemanager" &
+$SSH_CMD $MASTERNODE "$YARN_CONFIG $HADOOP_CONF_DIR --daemon start resourcemanager"
 
 for slave in $SLAVES
 do
 	m_echo "Starting NodeManager:" $slave
-	$SSH_CMD $slave "$YARN_CONFIG $HADOOP_CONF_DIR --daemon start nodemanager" &
+	$SSH_CMD $slave "$YARN_CONFIG $HADOOP_CONF_DIR --daemon start nodemanager"
 done
 
 if [[ $TIMELINE_SERVER == "true" ]]
 then
-        #YARN Timeline server
+    #YARN Timeline server
 	m_echo "Starting YARN Timeline server:" $MASTERNODE
-        $SSH_CMD $MASTERNODE "$YARN_CONFIG $HADOOP_CONF_DIR --daemon start timelineserver" &
+    $SSH_CMD $MASTERNODE "$YARN_CONFIG $HADOOP_CONF_DIR --daemon start timelineserver"
 fi
 
 if [[ $MR_JOBHISTORY_SERVER == "true" ]]
 then
 	#MapReduce history server
 	m_echo "Starting MapReduce history server:" $MASTERNODE
-	$SSH_CMD $MASTERNODE "$HADOOP_HOME/bin/mapred --config $HADOOP_CONF_DIR --daemon start historyserver" &
+	$SSH_CMD $MASTERNODE "$HADOOP_HOME/bin/mapred --config $HADOOP_CONF_DIR --daemon start historyserver"
 fi
