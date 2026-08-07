77019b3ba391e075147b3f12b2b91aa498e739bb
Roberto R. Exposito <rreye@udc.es>
2026-08-08 00:49:15 +0200

    Silence warnings when using Java>=11 with Hadoop 2.x

--- solutions/templates/Hadoop-YARN/hadoop-env.sh
+++ solutions/templates/Hadoop-YARN/hadoop-env.sh
@@ -66,7 +66,17 @@ done
 # export HADOOP_OPTS="-Djava.net.preferIPv4Stack=true -Dsun.security.krb5.debug=true -Dsun.security.spnego.debug"
 export HADOOPHOSTNAME=`$method_bin_dir/$hostname_script $hostfile $loopback_ip`
 export HADOOP_OPTS="-Djava.net.preferIPv4Stack=true -Djava.io.tmpdir=$tmp_dir -DHADOOPHOSTNAME=${HADOOPHOSTNAME}"
-export HADOOP_OPTS="$HADOOP_OPTS --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED"
+
+JAVA_BIN="${JAVA_HOME:+$JAVA_HOME/bin/}java"
+JAVA_VER_STR=$("$JAVA_BIN" -version 2>&1 | head -n 1 | cut -d '"' -f 2)
+JAVA_MAJOR_VER=$(echo "$JAVA_VER_STR" | awk -F '.' '{print ($1 == "1") ? $2 : $1}')
+
+if [ -n "$JAVA_MAJOR_VER" ] && [ "$JAVA_MAJOR_VER" -ge 9 ] 2>/dev/null; then
+    _ADD_OPENS="--add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED"
+    
+    export HADOOP_OPTS="$HADOOP_OPTS $_ADD_OPENS"
+fi
+
 
 # Command specific options appended to HADOOP_OPTS when specified
 export HADOOP_NAMENODE_OPTS="-Dhadoop.security.logger=${HADOOP_SECURITY_LOGGER:-INFO,RFAS} -Dhdfs.audit.logger=${HDFS_AUDIT_LOGGER:-INFO,NullAppender} $HADOOP_NAMENODE_OPTS"
