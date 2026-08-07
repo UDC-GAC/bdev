58bd99348e1b73f82fd4ab187bb344126b5e9ae3
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 22:56:38 +0200

    Fix comments and improve logging in start_hadoop_yarn_2.sh

--- solutions/src/common/bin/start_hadoop_yarn_2.sh
+++ solutions/src/common/bin/start_hadoop_yarn_2.sh
@@ -7,7 +7,8 @@ if [[ $FORCE_FORMAT_HDFS == "true" ]]; then
 	$SSH_CMD $MASTERNODE "$HDFS_CONFIG $HADOOP_CONF_DIR namenode -format -force -clusterID CID-bdev" > $HDFS_FORMAT_LOG 2>&1
 fi
 
-#Namenode & Datanodes
+#Namenode and Secondary NameNode (optionaal)
+m_echo "Starting NameNode and DataNodes"
 $SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode"
 
 if [[ $SECONDARY_NAMENODE == "true" ]]
@@ -16,20 +17,24 @@ then
 	$SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start secondarynamenode"
 fi
 
+#Datanodes
 $HADOOP_HOME/sbin/hadoop-daemons.sh --config $HADOOP_CONF_DIR --script hdfs start datanode
 
 #Resourcemanager & Nodemanagers
+m_echo "Starting Resourcemanager and Nodemanagers"
 $SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager"
 $HADOOP_HOME/sbin/yarn-daemons.sh --config $HADOOP_CONF_DIR start nodemanager
 
 if [[ $TIMELINE_SERVER == "true" ]]
 then
     #YARN Timeline server
+	m_echo "Starting YARN Timeline server"
 	$SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start timelineserver"
 fi
 
 if [[ $MR_JOBHISTORY_SERVER == "true" ]]
 then
 	#MapReduce history server
+	m_echo "Starting MapReduce history server"
 	$SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start historyserver"
 fi
