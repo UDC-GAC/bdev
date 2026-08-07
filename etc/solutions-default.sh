9ba1816687a26ff7435620a148740a5f6c211c8a
Roberto R. Exposito <rreye@udc.es>
2026-08-07 12:01:44 +0200

    Remove legacy frameworks

--- etc/solutions-default.sh
+++ etc/solutions-default.sh
@@ -2,22 +2,7 @@
 
 ## Configuration parameters for the frameworks
 
-#FLAME-MR
-export FLAMEMR_HADOOP_HOME=${SOLUTIONS_DIST_DIR}/Hadoop-YARN/2.10.2
-export FLAMEMR_WORKERS_PER_NODE=1 # Number of workers per node
-export FLAMEMR_CORES_PER_WORKER=`op_int "$CORES_PER_NODE / $FLAMEMR_WORKERS_PER_NODE"` # Number of cores per Worker
-export FLAMEMR_WORKER_MEMORY_FACTOR=0.90 # Percentage of the Worker memory allocated to heap
-export FLAMEMR_WORKER_MEMORY=`op_int "($NODEMANAGER_MEMORY / $FLAMEMR_WORKERS_PER_NODE) * $FLAMEMR_WORKER_MEMORY_FACTOR"` # Worker heapsize
-export FLAMEMR_BUFFER_SIZE=1048576 # Memory buffer size
-export FLAMEMR_DEBUG_MODE=false # Debug mode 
-export FLAMEMR_ITERATIVE_MODE=false # Iterative mode (caches intermediate results)
-export FLAMEMR_ITERATIVE_CACHE_INPUT=false # Include input caching in iterative mode
-export FLAMEMR_MERGE_OUTPUTS=1 # Number of merge outputs pipelined to the reduce phase
-export FLAMEMR_LOAD_BALANCING_MODE=false # Split large reduce partitions to avoid workload unbalance
-export FLAMEMR_LOAD_BALANCING_THRESHOLD=0 # Maximum partition size
-export FLAMEMR_ADDITIONAL_CONFIGURATION="" # Additional Flame-MR configuration
-
-# RDMA-Hadoop/RDMA-Hadoop-2/RDMA-Hadoop-3
+# RDMA-Hadoop-3
 export RDMA_HADOOP_IB_ENABLED=true # Enable RDMA connections through InfiniBand (IB)
 export RDMA_HADOOP_ROCE_ENABLED=false # Enable RDMA connections through RDMA over Converged Ethernet (RoCE)
 export RDMA_HADOOP_DFS_MEMORY_PERCENTAGE=0.7 # Threshold for RAM Disk usage
@@ -27,7 +12,7 @@ export RDMA_HADOOP_DISK_SHUFFLE_ENABLED="true" # Enable disk-based shuffle
 
 # Spark (common)
 export SPARK_HADOOP_HOME=${SOLUTIONS_DIST_DIR}/Hadoop-YARN/3.4.3
-export SPARK_SCALA_VERSION=2.12	# Scala version used by your Spark distribution
+export SPARK_SCALA_VERSION=2.11	# Scala version used by your Spark distribution
 export SPARK_DRIVER_CORES=1 # Number of cores for the driver
 export SPARK_DRIVER_MEMORY=`op_int "$CONTAINER_MEMORY * $SPARK_DRIVER_CORES"` # Amount of memory allocated to the driver
 export SPARK_DRIVER_HEAPSIZE_FACTOR=0.90 # Percentage of the driver memory allocated to heap
@@ -85,11 +70,6 @@ export SPARK_YARN_EXECUTOR_HEAPSIZE=`op_int "$SPARK_YARN_EXECUTOR_MEMORY * $SPAR
 export SPARK_YARN_EXECUTOR_MEMORY_OVERHEAD=`op_int "$SPARK_YARN_EXECUTOR_MEMORY * $SPARK_YARN_EXECUTOR_MEMORY_OVERHEAD_FACTOR"`  # Overhead of the Executor memory
 export SPARK_YARN_EXECUTOR_MEMORY_OVERHEAD=$(($SPARK_YARN_EXECUTOR_MEMORY_OVERHEAD>384?$SPARK_YARN_EXECUTOR_MEMORY_OVERHEAD:384))
 
-# RDMA-Spark
-export RDMA_SPARK_IB_ENABLED=true		# Enable RDMA connections through InfiniBand (IB)
-export RDMA_SPARK_ROCE_ENABLED=false		# Enable RDMA connections through RDMA over Converged Ethernet (RoCE)
-export RDMA_SPARK_SHUFFLE_CHUNK_SIZE=524288	# Chunk size for shuffle
-
 # Flink (common)
 export FLINK_HADOOP_HOME=${SOLUTIONS_DIST_DIR}/Hadoop-YARN/3.4.3
 export FLINK_SCALA_VERSION=2.12	# Scala version used by your Flink distribution
@@ -122,15 +102,6 @@ export FLINK_TASKMANAGER_MEMORY=`op_int "($MEMORY_ALLOC_PER_NODE - $FLINK_MEMORY
 # Flink on YARN
 export FLINK_YARN_JOBMANAGER_MEMORY=$APP_MASTER_MEMORY	# Memory allocated to the JobManager
 export FLINK_YARN_TASKMANAGER_MEMORY=`op_int "(($NODEMANAGER_MEMORY - $APP_MASTER_MEMORY) / $FLINK_TASKMANAGERS_PER_NODE) - $NODEMANAGER_INCREMENT_ALLOCATION"` # Memory allocated to each TaskManager
-
-# DataMPI
-export DATAMPI_HADOOP_HOME=${SOLUTIONS_DIST_DIR}/Hadoop/1.2.1
-export DATAMPI_TASK_HEAPSIZE_FACTOR=0.90 # Percentage of the task memory allocated to heap
-export DATAMPI_TASK_HEAPSIZE=`op_int "$NODEMANAGER_MEMORY * $DATAMPI_TASK_HEAPSIZE_FACTOR"` # Task heapsize
-
-# Mellanox UDA library
-export UDA_VERSION=3.4.1 # UDA library version
-export UDA_LIB_DIR=$SOLUTIONS_LIB_DIR/uda-$UDA_VERSION # UDA library directory
 
 # Apache Mahout
 export MAHOUT_HEAPSIZE=1024		# Heap size for Mahout master process
