17d538f7c4668ad0deac632dbb4c117dfd3b82fd
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-07 11:41:08 +0200

    Remove background execution from Hadoop start script

--- solutions/src/common/bin/start_hadoop_yarn_2.sh
+++ solutions/src/common/bin/start_hadoop_yarn_2.sh
@@ -8,21 +8,21 @@ if [[ $FORCE_FORMAT_HDFS == "true" ]]; then
 fi
 
 #Namenode & Datanodes
-$SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode" &
-$HADOOP_HOME/sbin/hadoop-daemons.sh --config $HADOOP_CONF_DIR --script hdfs start datanode &
+$SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode"
+$HADOOP_HOME/sbin/hadoop-daemons.sh --config $HADOOP_CONF_DIR --script hdfs start datanode
 
 #Resourcemanager & Nodemanagers
-$SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager" &
-$HADOOP_HOME/sbin/yarn-daemons.sh --config $HADOOP_CONF_DIR start nodemanager &
+$SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager"
+$HADOOP_HOME/sbin/yarn-daemons.sh --config $HADOOP_CONF_DIR start nodemanager
 
 if [[ $TIMELINE_SERVER == "true" ]]
 then
-        #YARN Timeline server
-	$SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start timelineserver" &
+    #YARN Timeline server
+	$SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start timelineserver"
 fi
 
 if [[ $MR_JOBHISTORY_SERVER == "true" ]]
 then
 	#MapReduce history server
-	$SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start historyserver" &
+	$SSH_CMD $MASTERNODE "$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start historyserver"
 fi
