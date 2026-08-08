4ab0c79878fa12510f7d8bb4098f74dc4191e777
Roberto R. Exposito <rreye@udc.es>
2026-08-08 19:00:11 +0200

    Set Hadoop 3.5.0 as default for Spark and Flink

--- etc/frameworks-conf.sh
+++ etc/frameworks-conf.sh
@@ -11,7 +11,7 @@ export RDMA_HADOOP_DFS_SSD_USED=false	# Enable SSD-oriented optimizations
 export RDMA_HADOOP_DISK_SHUFFLE_ENABLED="true" # Enable disk-based shuffle
 
 # Spark (common)
-export SPARK_HADOOP_HOME=${FRAMEWORKS_DIR}/Hadoop-YARN/3.4.3
+export SPARK_HADOOP_HOME=${FRAMEWORKS_DIR}/Hadoop-YARN/3.5.0
 export SPARK_SCALA_VERSION=2.12	# Scala version used by your Spark distribution
 export SPARK_DRIVER_CORES=1 # Number of cores for the driver
 export SPARK_DRIVER_MEMORY=`op_int "$CONTAINER_MEMORY * $SPARK_DRIVER_CORES"` # Amount of memory allocated to the driver
@@ -71,7 +71,7 @@ export SPARK_YARN_EXECUTOR_MEMORY_OVERHEAD=$(($SPARK_YARN_EXECUTOR_MEMORY_OVERHE
 export SPARK_YARN_EXECUTOR_HEAPSIZE=`op_int "$SPARK_YARN_EXECUTOR_MEMORY - $SPARK_YARN_EXECUTOR_MEMORY_OVERHEAD"` # Executor heapsize
 
 # Flink (common)
-export FLINK_HADOOP_HOME=${FRAMEWORKS_DIR}/Hadoop-YARN/3.4.3
+export FLINK_HADOOP_HOME=${FRAMEWORKS_DIR}/Hadoop-YARN/3.5.0
 export FLINK_SCALA_VERSION=2.12	# Scala version used by your Flink distribution
 export FLINK_LOCAL_DIRS=$LOCAL_DIRS # Comma-separated list of directories to use for local data
 export FLINK_HISTORY_SERVER=false # Start the Flink HistoryServer
