6723bc6d9df9a6a2d09aaecc4d33e501ec66c0b1
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-11 00:29:04 +0200

    Fix echo statement for NFS mount point

--- bin/functions.sh
+++ bin/functions.sh
@@ -48,7 +48,7 @@ function m_start_message()
 	m_echo "Cluster sizes ($NUM_CLUSTERS): $CLUSTER_SIZES"
 	m_echo "Storage backend: $STORAGE_BACKEND"
 	if [ "${STORAGE_BACKEND,,}" == "nfs" ]; then
-		REPORT="$REPORT \t NFS mount point  \t\t\t $NFS_MOUNT_POINT \n"
+		m_echo NFS mount point: $NFS_MOUNT_POINT"
 	fi
 	m_echo "JVM: $LOAD_JAVA_COMMAND"
 }
