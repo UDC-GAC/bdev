de9e88ce1b1d242c17d8b1cee403af55048e72b9
Roberto R. Exposito <rreye@udc.es>
2026-08-11 00:12:17 +0200

    Fix if statements

--- frameworks/benchmarks/Spark/bin/prepare.sh
+++ frameworks/benchmarks/Spark/bin/prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -v FINISH_YARN && "$FINISH_YARN" == "true" ]; then
+if [ -v FINISH_YARN ] && [ "$FINISH_YARN" == "true" ]; then
 	export FINISH_YARN_FORCE=true
 	${COMMON_SRC_DIR}/bin/finish_yarn.sh
 fi
