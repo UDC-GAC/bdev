cd0c205e33a817cccbe3646c45c941bd72baaf43
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-10 18:47:04 +0200

    Replace HDFS command with storage_rm

--- frameworks/benchmarks/Hadoop/bin/tpcx_hs/run.sh
+++ frameworks/benchmarks/Hadoop/bin/tpcx_hs/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-${HDFS_CMD} ${RMR} $OUTPUT_TPCX_HS
+storage_rm -R ${OUTPUT_TPCX_HS}
 
 run_benchmark "$HADOOP_EXECUTABLE jar $TPCX_HS_JAR es.udc.tpcx_hs.hadoop.HSSort \
 	-D $CONFIG_REDUCER_NUMBER=${REDUCERS_NUMBER} $INPUT_TPCX_HS $OUTPUT_TPCX_HS"
