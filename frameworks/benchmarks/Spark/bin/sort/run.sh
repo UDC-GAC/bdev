bd5c47a1816eac6b4d9a60f28089605634af45cd
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-10 18:42:14 +0200

    Replace HDFS command with storage_rm

--- frameworks/benchmarks/Spark/bin/sort/run.sh
+++ frameworks/benchmarks/Spark/bin/sort/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-${HDFS_CMD} ${RMR} $OUTPUT_SORT
+storage_rm -R ${OUTPUT_SORT}
 
 run_benchmark "$SPARK_HOME/bin/spark-submit \
 	--class es.udc.gac.sparkbench.ScalaSort ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
