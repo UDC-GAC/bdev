7ffd2eb534213f4ee3e45a8336cdc9a661ea4462
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-10 18:42:30 +0200

    Replace HDFS command with storage_rm

--- frameworks/benchmarks/Spark/bin/terasort/run.sh
+++ frameworks/benchmarks/Spark/bin/terasort/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-${HDFS_CMD} ${RMR} $OUTPUT_TERASORT
+storage_rm -R ${OUTPUT_TERASORT}
 
 run_benchmark "$SPARK_HOME/bin/spark-submit \
 	--class es.udc.gac.sparkbench.ScalaTeraSort ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
