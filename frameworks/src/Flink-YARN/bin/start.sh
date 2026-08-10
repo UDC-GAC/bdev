b60a49b2c7602cec0356974a47785d892b16900a
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-10 18:57:11 +0200

    Refactor history server directory commands

--- frameworks/src/Flink-YARN/bin/start.sh
+++ frameworks/src/Flink-YARN/bin/start.sh
@@ -43,11 +43,10 @@ fi
 m_echo "YARN appID session: $YARN_APP_ID"
 export DEPLOY_ARGS="$DEPLOY_ARGS -Dyarn.application.id=$YARN_APP_ID"
 
-if [ $FLINK_HISTORY_SERVER == "true" ]
-then
-        ${HDFS_CMD} ${MKDIR} ${FLINK_HISTORY_SERVER_DIR}
-        ${HDFS_CMD} ${CHMOD} 777 ${FLINK_HISTORY_SERVER_DIR}
+if [ $FLINK_HISTORY_SERVER == "true" ]; then
+    storage_mkdir ${FLINK_HISTORY_SERVER_DIR}
+	storage_chmod -R 777 ${FLINK_HISTORY_SERVER_DIR}
 
-        #Flink history server
-        $FLINK_HOME/bin/historyserver.sh start
+    #Flink history server
+    $FLINK_HOME/bin/historyserver.sh start
 fi
