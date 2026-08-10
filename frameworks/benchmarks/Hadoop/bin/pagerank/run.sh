218ae58ab774c0e97ade8bfcbf0092736019b39c
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-10 18:45:30 +0200

    Fix command to remove output directory

--- frameworks/benchmarks/Hadoop/bin/pagerank/run.sh
+++ frameworks/benchmarks/Hadoop/bin/pagerank/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-${HDFS_CMD} ${RMR} $OUTPUT_PAGERANK
+storage_rm -R ${OUTPUT_PAGERANK}
 
 run_benchmark "$HADOOP_EXECUTABLE jar ${PEGASUS_JAR} pegasus.PagerankNaive \
 		${INPUT_PAGERANK}/edges ${OUTPUT_PAGERANK} \
