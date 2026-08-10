c021f050478eab78059aa67aed6aab9095c22a70
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-10 18:56:16 +0200

    Update start.sh for Spark history server setup
    Refactor Spark history server directory creation and permissions.

--- frameworks/src/Spark-YARN/bin/start.sh
+++ frameworks/src/Spark-YARN/bin/start.sh
@@ -2,10 +2,9 @@
 
 $COMMON_SRC_DIR/bin/start_hadoop_yarn.sh
 
-if [ $SPARK_HISTORY_SERVER == "true" ]
-then
-        ${HDFS_CMD} ${MKDIR} ${SPARK_HISTORY_SERVER_DIR}
-        ${HDFS_CMD} ${CHMOD} 777 ${SPARK_HISTORY_SERVER_DIR}
+if [ $SPARK_HISTORY_SERVER == "true" ]; then
+        storage_mkdir ${SPARK_HISTORY_SERVER_DIR}
+	storage_chmod -R 777 ${SPARK_HISTORY_SERVER_DIR}
 
         #Spark history server
         $SPARK_HOME/sbin/start-history-server.sh
