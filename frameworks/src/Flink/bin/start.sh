01a45462cbb97002d59121784b7a89a6da5ba6b0
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-11 01:40:14 +0200

    Fix formatting in start.sh for Flink cluster startup

--- frameworks/src/Flink/bin/start.sh
+++ frameworks/src/Flink/bin/start.sh
@@ -17,9 +17,9 @@ m_echo "Starting the standalone Flink cluster (Session Mode)"
 $FLINK_HOME/bin/start-cluster.sh
 
 if [ $FLINK_HISTORY_SERVER == "true" ]; then
-        storage_mkdir ${FLINK_HISTORY_SERVER_DIR}
+    storage_mkdir ${FLINK_HISTORY_SERVER_DIR}
 	storage_chmod -R 777 ${FLINK_HISTORY_SERVER_DIR}
 
-        #Flink history server
-        $FLINK_HOME/bin/historyserver.sh start
+    #Flink history server
+    $FLINK_HOME/bin/historyserver.sh start
 fi
