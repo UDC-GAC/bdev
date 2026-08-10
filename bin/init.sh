46f3bc271465058c12f31d94364df36c9e4997fa
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-10 19:25:30 +0200

    Load storage backend functions in init script

--- bin/init.sh
+++ bin/init.sh
@@ -2,6 +2,13 @@
 
 m_echo "Initializing"
 
+# Load nodes and IPs
+. $BDEV_BIN_DIR/load-nodes.sh
+
+# Load storage backend functions
+. $COMMON_SRC_DIR/lib/storage_backend.sh
+export STORAGE_BACKEND_URI=$(get_storage_uri_prefix)
+
 . $CLEAN_DAEMONS_SCRIPT
 
 begin_report
