7901157712b4d3b2244359208767b4bb565ee6a2
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-10 18:41:23 +0200

    Replace HDFS command with storage_rm

--- frameworks/benchmarks/Spark/bin/kmeans/run.sh
+++ frameworks/benchmarks/Spark/bin/kmeans/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-${HDFS_CMD} ${RMR} $OUTPUT_KMEANS
+storage_rm -R ${OUTPUT_KMEANS}
 
 run_benchmark "$SPARK_HOME/bin/spark-submit \
 	--class es.udc.gac.sparkbench.ScalaMLlibDenseKMeans ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
