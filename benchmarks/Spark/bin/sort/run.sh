31e35d40742fac42ab3ee0dc933b0ed9e0506778
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-11 12:33:49 +0200

    Fix class reference in Spark benchmark run script

--- benchmarks/Spark/bin/sort/run.sh
+++ benchmarks/Spark/bin/sort/run.sh
@@ -3,7 +3,7 @@
 storage_rm -R ${OUTPUT_SORT}
 
 run_benchmark "$SPARK_HOME/bin/spark-submit \
-	--class es.udc.gac.sparkbench.ScalaSort ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
+	--class es.udc.gac.sparkbench.${SPARK_API}.ScalaSort ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
 	$INPUT_SORT $OUTPUT_SORT $EXAMPLES_DATA_FORMAT"
 
 if [ $(cat $TMPLOGFILE | grep -i -E "final status: FAILED" | wc -l) != "0" ]
