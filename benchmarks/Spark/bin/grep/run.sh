1bae22fb8b9cd44d6597c8261429ff51fa88721b
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-11 12:30:13 +0200

    Update Spark class reference in run.sh script

--- benchmarks/Spark/bin/grep/run.sh
+++ benchmarks/Spark/bin/grep/run.sh
@@ -3,7 +3,7 @@
 storage_rm -R ${OUTPUT_GREP}
 
 run_benchmark "$SPARK_HOME/bin/spark-submit \
-	--class es.udc.gac.sparkbench.ScalaGrep ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
+	--class es.udc.gac.sparkbench.${SPARK_API}.ScalaGrep ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
 	$INPUT_GREP $OUTPUT_GREP $GREP_REGEX $EXAMPLES_DATA_FORMAT"
 
 if [ $(cat $TMPLOGFILE | grep -i -E "final status: FAILED" | wc -l) != "0" ]
