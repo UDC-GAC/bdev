a8724cdaa0b34d59b7819f7601f63aabce9bba84
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-11 12:31:56 +0200

    Fix class reference in KMeans run script

--- benchmarks/Spark/bin/kmeans/run.sh
+++ benchmarks/Spark/bin/kmeans/run.sh
@@ -3,7 +3,7 @@
 storage_rm -R ${OUTPUT_KMEANS}
 
 run_benchmark "$SPARK_HOME/bin/spark-submit \
-	--class es.udc.gac.sparkbench.ScalaMLlibDenseKMeans ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
+	--class es.udc.gac.sparkbench.${SPARK_API}.ScalaMLlibDenseKMeans ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
 	--input ${INPUT_KMEANS}/samples --centers ${INPUT_KMEANS}/cluster --output ${OUTPUT_KMEANS} \
 	--numIterations ${KMEANS_MAX_ITERATIONS} --convergenceDelta ${KMEANS_CONVERGENCE_DELTA}"
 
