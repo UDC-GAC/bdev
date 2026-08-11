5425c554206d1e6069fbac898219a7d808b4869e
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-11 12:28:35 +0200

    Update class reference for NaiveBayes in run.sh

--- benchmarks/Spark/bin/bayes/run.sh
+++ benchmarks/Spark/bin/bayes/run.sh
@@ -3,7 +3,7 @@
 storage_rm -R ${OUTPUT_BAYES}
 
 run_benchmark "$SPARK_HOME/bin/spark-submit \
-	--class es.udc.gac.sparkbench.ScalaMLlibSparseNaiveBayes ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
+	--class es.udc.gac.sparkbench.${SPARK_API}.ScalaMLlibSparseNaiveBayes ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
 	${INPUT_BAYES} ${OUTPUT_BAYES}/model"
 
 if [ $(cat $TMPLOGFILE | grep -i -E "final status: FAILED" | wc -l) != "0" ]
