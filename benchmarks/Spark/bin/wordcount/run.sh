d80639c3ededcf7d6b90f00f618328c2bbac6f40
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-11 12:35:57 +0200

    Update Spark job class reference in run.sh

--- benchmarks/Spark/bin/wordcount/run.sh
+++ benchmarks/Spark/bin/wordcount/run.sh
@@ -3,7 +3,7 @@
 storage_rm -R ${OUTPUT_WORDCOUNT}
 
 run_benchmark "$SPARK_HOME/bin/spark-submit \
-	--class es.udc.gac.sparkbench.ScalaWordCount ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
+	--class es.udc.gac.sparkbench.${SPARK_API}.ScalaWordCount ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
 	$INPUT_WORDCOUNT $OUTPUT_WORDCOUNT $EXAMPLES_DATA_FORMAT"
 
 if [ $(cat $TMPLOGFILE | grep -i -E "final status: FAILED" | wc -l) != "0" ]
