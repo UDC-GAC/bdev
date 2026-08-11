db4deb7299478b0be00b941197384f9bab9bf8d3
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-11 12:30:23 +0200

    Fix class name for Spark connected components benchmark

--- benchmarks/Spark/bin/connected_components/run.sh
+++ benchmarks/Spark/bin/connected_components/run.sh
@@ -3,7 +3,7 @@
 storage_rm -R ${OUTPUT_CC}
 
 run_benchmark "$SPARK_HOME/bin/spark-submit \
-	--class es.udc.gac.sparkbench.ScalaGraphXConnectedComponents ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
+	--class es.udc.gac.sparkbench.rdd.ScalaGraphXConnectedComponents ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
 	${INPUT_CC}/edges ${OUTPUT_CC} ${CC_PAGES} ${CC_MAX_ITERATIONS}"
 
 if [ $(cat $TMPLOGFILE | grep -i -E "final status: FAILED" | wc -l) != "0" ]
