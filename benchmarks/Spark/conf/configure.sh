16f69229a80b48261c2025e4a48a6a7d785dde5b
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-13 18:24:56 +0200

    Rename SPARK_TCPX_HS_JAR_NAME to SPARK_TPCX_HS_JAR_NAME

--- benchmarks/Spark/conf/configure.sh
+++ benchmarks/Spark/conf/configure.sh
@@ -4,20 +4,20 @@ export SORT_PARTITIONS=$(($SPARK_EXECUTORS * $SPARK_CORES_PER_EXECUTOR))
 
 if [ $SPARK_SERIES == "2" ]; then
 	export SPARK_BENCH_JAR_NAME=sparkbench-2.4_${SPARK_SCALA_VERSION}.jar
-	export SPARK_TCPX_HS_JAR_NAME=tpcx-hs-spark-2.4.0_${SPARK_SCALA_VERSION}.jar
+	export SPARK_TPCX_HS_JAR_NAME=tpcx-hs-spark-2.4.0_${SPARK_SCALA_VERSION}.jar
 else
 	case "$SPARK_MAJOR_VERSION" in
    		3.0|3.1)
         	export SPARK_BENCH_JAR_NAME=sparkbench-3.0_${SPARK_SCALA_VERSION}.jar
-			export SPARK_TCPX_HS_JAR_NAME=tpcx-hs-spark-3.0.0_${SPARK_SCALA_VERSION}.jar
+			export SPARK_TPCX_HS_JAR_NAME=tpcx-hs-spark-3.0.0_${SPARK_SCALA_VERSION}.jar
         	;;
     	3.2|3.3|3.4|3.5)
         	export SPARK_BENCH_JAR_NAME=sparkbench-3.2_${SPARK_SCALA_VERSION}.jar
-			export SPARK_TCPX_HS_JAR_NAME=tpcx-hs-spark-3.2.0_${SPARK_SCALA_VERSION}.jar
+			export SPARK_TPCX_HS_JAR_NAME=tpcx-hs-spark-3.2.0_${SPARK_SCALA_VERSION}.jar
         	;;
     	4.0|4.1|4.2)
 		    export SPARK_BENCH_JAR_NAME=sparkbench-4.0_${SPARK_SCALA_VERSION}.jar
-			export SPARK_TCPX_HS_JAR_NAME=tpcx-hs-spark-4.0.0_${SPARK_SCALA_VERSION}.jar
+			export SPARK_TPCX_HS_JAR_NAME=tpcx-hs-spark-4.0.0_${SPARK_SCALA_VERSION}.jar
         	;;
 		*)
 			m_exit "Spark version is not supported: $SPARK_VERSION"
@@ -45,18 +45,18 @@ else
 fi
 
 if [ "$BENCHMARK" == "tpcx_hs" ]; then
-	export TCPX_HS_JAR=$SPARK_BENCH_DIR/$SPARK_TCPX_HS_JAR_NAME
+	export TPCX_HS_JAR=$SPARK_BENCH_DIR/$SPARK_TPCX_HS_JAR_NAME
 
 	# Download TPCx-HS jar file
 	URL=https://bdev.des.udc.es/dist/tpcx-hs
-	m_echo "Downloading $SPARK_TCPX_HS_JAR_NAME from $URL"
+	m_echo "Downloading $SPARK_TPCX_HS_JAR_NAME from $URL"
 
-    wget -q -O $TCPX_HS_JAR $URL/$SPARK_TCPX_HS_JAR_NAME
+    wget -q -O $TPCX_HS_JAR $URL/$SPARK_TPCX_HS_JAR_NAME
 
     if [ $? != 0 ]; then
-		rm $TCPX_HS_JAR >& /dev/null
-		m_exit "Error when downloading $SPARK_TCPX_HS_JAR_NAME"
+		rm $TPCX_HS_JAR >& /dev/null
+		m_exit "Error when downloading $SPARK_TPCX_HS_JAR_NAME"
     fi
 else
-	m_echo "Using $TCPX_HS_JAR"
+	m_echo "Using $TPCX_HS_JAR"
 fi
