5a4c846982738cfd72fcd1893337807339af8abb
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-13 18:04:08 +0200

    Refactor configure.sh for bash and TPCx-HS support
    Updated the script to use bash instead of sh, improved conditional syntax, and added support for downloading TPCx-HS jar file.

--- benchmarks/Hadoop/conf/configure.sh
+++ benchmarks/Hadoop/conf/configure.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 export PEGASUS_JAR=$THIRD_PARTY_DIR/pegasus-2.0/pegasus-2.0.jar
 export HIVE_HOME=$THIRD_PARTY_DIR/hive-$HIVE_VERSION
 
-if [[ ( $GEN_KMEANS == "true" || $GEN_BAYES == "true" ) && ! -d $MAHOUT_HOME ]]
-then
+if [[ ( $GEN_KMEANS == "true" || $GEN_BAYES == "true" ) && ! -d $MAHOUT_HOME ]]; then
 	URL="https://archive.apache.org/dist/mahout"
 
 	# Download Mahout 0.11.1 and 0.12.0 compiled for Hadoop 1 from bdev
@@ -24,8 +23,7 @@ then
 	m_echo "Downloading mahout-$MAHOUT_VERSION"
 	wget -q -O $TMP_MAHOUT_FILE $URL/$MAHOUT_VERSION/apache-mahout-distribution-${MAHOUT_VERSION}.tar.gz
 
-	if [[ $? != 0 ]]
-	then
+	if [[ $? != 0 ]]; then
 		rm $TMP_MAHOUT_FILE
 		TMP_MAHOUT_FILE=$THIRD_PARTY_DIR/mahout-distribution-${MAHOUT_VERSION}.tar.gz
 		TMP_MAHOUT_DIR=$THIRD_PARTY_DIR/mahout-distribution-${MAHOUT_VERSION}
@@ -38,8 +36,7 @@ then
 	rm $TMP_MAHOUT_FILE
 fi
 
-if [[ ( $GEN_AGGREGATION == "true" || $GEN_JOIN == "true" || $GEN_SCAN == "true" ) && ! -d $HIVE_HOME ]]
-then
+if [[ ( $GEN_AGGREGATION == "true" || $GEN_JOIN == "true" || $GEN_SCAN == "true" ) && ! -d $HIVE_HOME ]]; then
 	URL="https://archive.apache.org/dist/hive/"
 	TMP_HIVE_FILE=$THIRD_PARTY_DIR/apache-hive-${HIVE_VERSION}-bin.tar.gz
 	TMP_HIVE_DIR=$THIRD_PARTY_DIR/apache-hive-${HIVE_VERSION}-bin
@@ -47,8 +44,7 @@ then
 	m_echo "Downloading hive-$HIVE_VERSION"
 	wget -q -O $TMP_HIVE_FILE $URL/hive-$HIVE_VERSION/apache-hive-${HIVE_VERSION}-bin.tar.gz
 
-	if [[ $? != 0 ]]
-	then
+	if [[ $? != 0 ]]; then
 		rm $TMP_HIVE_FILE
 		TMP_HIVE_FILE=$THIRD_PARTY_DIR/hive-${HIVE_VERSION}-bin.tar.gz
 		TMP_HIVE_DIR=$THIRD_PARTY_DIR/hive-${HIVE_VERSION}-bin
@@ -59,4 +55,23 @@ then
 	tar -xzf $TMP_HIVE_FILE -C $THIRD_PARTY_DIR
 	mv $TMP_HIVE_DIR $HIVE_HOME
 	rm $TMP_HIVE_FILE
+fi
+
+export HADOOP_BENCH_DIR=$SOL_BENCH_DIR/bin
+export HADOOP_TCPX_HS_JAR_NAME=tpcx-hs-spark-4.0.0_2.13.jar
+export HADOOP_TCPX_HS_JAR=$HADOOP_BENCH_DIR/${HADOOP_TCPX_HS_JAR_NAME}
+
+if [[ "$BENCHMARK" == "tpcx_hs" ]]; then
+	# Download TPCx-HS jar file
+	URL=https://bdev.des.udc.es/dist/tpcx-hs
+	m_echo "Downloading $HADOOP_TCPX_HS_JAR_NAME from $URL"
+
+    wget -q -O $HADOOP_TCPX_HS_JAR $URL/$HADOOP_TCPX_HS_JAR_NAME
+
+    if [ $? != 0 ]; then
+		rm $HADOOP_TCPX_HS_JAR >& /dev/null
+		m_exit "Error when downloading $HADOOP_TCPX_HS_JAR_NAME"
+    fi
+else
+	m_echo "Using $HADOOP_TCPX_HS_JAR"
 fi
