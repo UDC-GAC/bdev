8ed7e03987a3ef3dbb3a3246a5c21a10b488f61c
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-13 18:05:39 +0200

    Remove leading spaces in SPARK_TCPX_HS_JAR_NAME exports

--- benchmarks/Spark/conf/configure.sh
+++ benchmarks/Spark/conf/configure.sh
@@ -4,20 +4,20 @@ export SORT_PARTITIONS=$(($SPARK_EXECUTORS * $SPARK_CORES_PER_EXECUTOR))
 
 if [ $SPARK_SERIES == "2" ]; then
 	export SPARK_BENCH_JAR_NAME=sparkbench-2.4_${SPARK_SCALA_VERSION}.jar
-	export SPARK_TCPX_HS_JAR_NAME= tpcx-hs-spark-2.4.0_${SPARK_SCALA_VERSION}.jar
+	export SPARK_TCPX_HS_JAR_NAME=tpcx-hs-spark-2.4.0_${SPARK_SCALA_VERSION}.jar
 else
 	case "$SPARK_MAJOR_VERSION" in
    		3.0|3.1)
         	export SPARK_BENCH_JAR_NAME=sparkbench-3.0_${SPARK_SCALA_VERSION}.jar
-			export SPARK_TCPX_HS_JAR_NAME= tpcx-hs-spark-3.0.0_${SPARK_SCALA_VERSION}.jar
+			export SPARK_TCPX_HS_JAR_NAME=tpcx-hs-spark-3.0.0_${SPARK_SCALA_VERSION}.jar
         	;;
     	3.2|3.3|3.4|3.5)
         	export SPARK_BENCH_JAR_NAME=sparkbench-3.2_${SPARK_SCALA_VERSION}.jar
-			export SPARK_TCPX_HS_JAR_NAME= tpcx-hs-spark-3.2.0_${SPARK_SCALA_VERSION}.jar
+			export SPARK_TCPX_HS_JAR_NAME=tpcx-hs-spark-3.2.0_${SPARK_SCALA_VERSION}.jar
         	;;
     	4.0|4.1|4.2)
 		    export SPARK_BENCH_JAR_NAME=sparkbench-4.0_${SPARK_SCALA_VERSION}.jar
-			export SPARK_TCPX_HS_JAR_NAME= tpcx-hs-spark-4.0.0_${SPARK_SCALA_VERSION}.jar
+			export SPARK_TCPX_HS_JAR_NAME=tpcx-hs-spark-4.0.0_${SPARK_SCALA_VERSION}.jar
         	;;
 		*)
 			m_exit "Spark version is not supported: $SPARK_VERSION"
@@ -44,7 +44,7 @@ else
 	m_echo "Using $SPARK_BENCH_JAR"
 fi
 
-export SPARK_TCPX_HS_JAR=$SPARK_BENCH_DIR/${SPARK_TCPX_HS_JAR_NAME}
+export SPARK_TCPX_HS_JAR=$SPARK_BENCH_DIR/$SPARK_TCPX_HS_JAR_NAME
 
 if [ "$BENCHMARK" == "tpcx_hs" ]; then
 	# Download TPCx-HS jar file
