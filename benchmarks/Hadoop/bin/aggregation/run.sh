9c91d2a15740377546d5d5d3bb6a2518468e119d
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-14 13:56:24 +0200

    Replace Hive command with Beeline command

--- benchmarks/Hadoop/bin/aggregation/run.sh
+++ benchmarks/Hadoop/bin/aggregation/run.sh
@@ -5,5 +5,5 @@ storage_rm -R ${OUTPUT_AGGREGATION}
 HIVE_SQL_FILE=${BENCHMARK_OUTPUT_DIR}/uservisits_aggregation.hive
 prepare_sql_aggregation ${HIVE_SQL_FILE}
 init_hive_metastore
-HIVE_COMMAND="${HIVE_HOME}/bin/hive ${HIVE_OPTS} -f ${HIVE_SQL_FILE}"
-run_benchmark "${HIVE_COMMAND}"
+
+run_benchmark "${HIVE_HOME}/bin/beeline -u jdbc:hive2:// ${HIVE_OPTS} -f ${HIVE_SQL_FILE}"
