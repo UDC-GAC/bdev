2474db29c8ad92727d6d2c29c12cbd5d495a82e2
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-14 11:55:52 +0200

    Refactor Hive version detection logic in configure.sh

--- benchmarks/Hadoop/conf/configure.sh
+++ benchmarks/Hadoop/conf/configure.sh
@@ -36,25 +36,31 @@ if [[ ( $GEN_KMEANS == "true" || $GEN_BAYES == "true" ) && ! -d $MAHOUT_HOME ]];
 	rm $TMP_MAHOUT_FILE
 fi
 
-if [[ ( $GEN_AGGREGATION == "true" || $GEN_JOIN == "true" || $GEN_SCAN == "true" ) && ! -d $HIVE_HOME ]]; then
-	URL="https://archive.apache.org/dist/hive/"
-	TMP_HIVE_FILE=$THIRD_PARTY_DIR/apache-hive-${HIVE_VERSION}-bin.tar.gz
-	TMP_HIVE_DIR=$THIRD_PARTY_DIR/apache-hive-${HIVE_VERSION}-bin
-
-	m_echo "Downloading hive-$HIVE_VERSION"
-	wget -q -O $TMP_HIVE_FILE $URL/hive-$HIVE_VERSION/apache-hive-${HIVE_VERSION}-bin.tar.gz
-
-	if [[ $? != 0 ]]; then
-		rm $TMP_HIVE_FILE
-		TMP_HIVE_FILE=$THIRD_PARTY_DIR/hive-${HIVE_VERSION}-bin.tar.gz
-		TMP_HIVE_DIR=$THIRD_PARTY_DIR/hive-${HIVE_VERSION}-bin
-		wget -q -O $TMP_HIVE_FILE $URL/hive-$HIVE_VERSION/hive-${HIVE_VERSION}-bin.tar.gz
+if [[ ( $GEN_AGGREGATION == "true" || $GEN_JOIN == "true" || $GEN_SCAN == "true" ) ]]; then
+	if [[ "${HADOOP_HIVE_VERSION}" == 4.* ]]; then
+		m_exit "Hive 4.x is not supported: $HADOOP_HIVE_VERSION"
 	fi
+	
+	if [[ ! -d $HIVE_HOME ]]; then
+		URL="https://archive.apache.org/dist/hive/"
+		TMP_HIVE_FILE=$THIRD_PARTY_DIR/apache-hive-${HIVE_VERSION}-bin.tar.gz
+		TMP_HIVE_DIR=$THIRD_PARTY_DIR/apache-hive-${HIVE_VERSION}-bin
 
-	m_echo "Extracting $TMP_HIVE_FILE"
-	tar -xzf $TMP_HIVE_FILE -C $THIRD_PARTY_DIR
-	mv $TMP_HIVE_DIR $HIVE_HOME
-	rm $TMP_HIVE_FILE
+		m_echo "Downloading hive-$HIVE_VERSION"
+		wget -q -O $TMP_HIVE_FILE $URL/hive-$HIVE_VERSION/apache-hive-${HIVE_VERSION}-bin.tar.gz
+
+		if [[ $? != 0 ]]; then
+			rm $TMP_HIVE_FILE
+			TMP_HIVE_FILE=$THIRD_PARTY_DIR/hive-${HIVE_VERSION}-bin.tar.gz
+			TMP_HIVE_DIR=$THIRD_PARTY_DIR/hive-${HIVE_VERSION}-bin
+			wget -q -O $TMP_HIVE_FILE $URL/hive-$HIVE_VERSION/hive-${HIVE_VERSION}-bin.tar.gz
+		fi
+
+		m_echo "Extracting $TMP_HIVE_FILE"
+		tar -xzf $TMP_HIVE_FILE -C $THIRD_PARTY_DIR
+		mv $TMP_HIVE_DIR $HIVE_HOME
+		rm $TMP_HIVE_FILE
+	fi
 fi
 
 if [[ "$GEN_TPCX_HS" == "true" ]]; then
