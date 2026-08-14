ab32e638ba2e8bdb5f11fc5c396ec84ea0083f5b
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-14 02:13:39 +0200

    Update Spark benchmark class for SQL scan

--- benchmarks/Spark/bin/scan/run.sh
+++ benchmarks/Spark/bin/scan/run.sh
@@ -6,5 +6,5 @@ HIVE_SQL_FILE=${BENCHMARK_OUTPUT_DIR}/uservisits_scan.hive
 prepare_sql_scan ${HIVE_SQL_FILE}
 
 run_benchmark "$SPARK_HOME/bin/spark-submit \
-        --class es.udc.gac.sparkbench.rdd.ScalaHiveSQL ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
+        --class es.udc.gac.sparkbench.sql.ScalaHiveSQL ${DEPLOY_ARGS} $SPARK_BENCH_JAR \
 	ScalaHiveSQLScan ${HIVE_SQL_FILE}"
