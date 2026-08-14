8cc588a1c23973e6f847690842edfc495738b86e
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-14 11:14:41 +0200

    Update run.sh to support Hive 4.x with Beeline

--- benchmarks/Hadoop/bin/join/run.sh
+++ benchmarks/Hadoop/bin/join/run.sh
@@ -6,4 +6,10 @@ HIVE_SQL_FILE=${BENCHMARK_OUTPUT_DIR}/rankings_uservisits_join.hive
 prepare_sql_join ${HIVE_SQL_FILE}
 init_hive_metastore
 
-run_benchmark "${HIVE_HOME}/bin/hive -f ${HIVE_SQL_FILE}"
+if [[ "${HADOOP_HIVE_VERSION}" == 4.* ]]; then
+    HIVE_COMMAND="${HIVE_HOME}/bin/beeline -u jdbc:hive2:// ${HIVE_OPTS} -f ${HIVE_SQL_FILE}"
+else
+    HIVE_COMMAND="${HIVE_HOME}/bin/hive ${HIVE_OPTS} -f ${HIVE_SQL_FILE}"
+fi
+
+run_benchmark "${HIVE_COMMAND}"
