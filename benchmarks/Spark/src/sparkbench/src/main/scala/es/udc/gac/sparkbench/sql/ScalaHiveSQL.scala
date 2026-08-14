13b5f4452d5d9b206f7aaa3a9bc82c6208980b80
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-14 02:17:14 +0200

    Refactor ScalaHiveSQL to use SparkSession

--- benchmarks/Spark/src/sparkbench/src/main/scala/es/udc/gac/sparkbench/sql/ScalaHiveSQL.scala
+++ benchmarks/Spark/src/sparkbench/src/main/scala/es/udc/gac/sparkbench/sql/ScalaHiveSQL.scala
@@ -1,12 +1,12 @@
 package es.udc.gac.sparkbench.sql
 
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
 /*
  * Adapted from HiBench
  */
-object ScalaHiveSQL{
+object ScalaHiveSQL {
 
   def main(args: Array[String]) {
 
@@ -17,29 +17,30 @@ object ScalaHiveSQL{
 
     val bench_name = args(0)
     val sql_file = args(1)
-    val sparkConf = new SparkConf().setAppName("SparkBench ScalaHiveSQL " + bench_name)
 
-    /* Hive configuration */
     val bench_output_dir = System.getenv("BENCHMARK_OUTPUT_DIR")
     val hive_tmp_dir = System.getenv("HIVE_TMP_DIR")
     val tmp_dir = System.getenv("TMP_DIR")
-    System.setProperty("javax.jdo.option.ConnectionURL", "jdbc:derby:;databaseName="+bench_output_dir+"/metastore_db;create=true")
-    System.setProperty("hive.exec.scratchdir", hive_tmp_dir);
-    System.setProperty("hive.exec.local.scratchdir", tmp_dir+"/hive");
-    System.setProperty("hive.input.format", "org.apache.hadoop.hive.ql.io.HiveInputFormat");
-    System.setProperty("hive.stats.autogather", "false");
-    System.setProperty("derby.stream.error.file", bench_output_dir+"/derby.log")
-    System.setProperty("hive.log.dir", tmp_dir+"/hive")
 
-    val sc = new SparkContext(sparkConf)
-    val hiveContext = new HiveContext(sc)
+    System.setProperty("javax.jdo.option.ConnectionURL", s"jdbc:derby:;databaseName=$bench_output_dir/metastore_db_spark;create=true")
+    System.setProperty("hive.exec.scratchdir", hive_tmp_dir)
+    System.setProperty("hive.exec.local.scratchdir", s"$tmp_dir/hive")
+    System.setProperty("hive.input.format", "org.apache.hadoop.hive.ql.io.HiveInputFormat")
+    System.setProperty("hive.stats.autogather", "false")
+    System.setProperty("derby.stream.error.file", s"$bench_output_dir/derby_spark.log")
+    System.setProperty("hive.log.dir", s"$tmp_dir/hive")
+
+    val session = SparkSession.builder()
+      .appName("SparkBench ScalaHiveSQL " + bench_name)
+      .enableHiveSupport()
+      .getOrCreate()
 
     val _sql = scala.io.Source.fromFile(sql_file).mkString
     _sql.split(';').foreach { x =>
       if (x.trim.nonEmpty)
-        hiveContext.sql(x)
+        session.sql(x)
     }
 
-    sc.stop()
+    session.stop()
   }
 }
