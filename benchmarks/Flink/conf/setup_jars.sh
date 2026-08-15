eaaaf03a553054974e9d908f4df72d4c68e463d9
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-15 16:49:34 +0200

    Modify Hive connector setup for Flink versions
    Update Flink SQL connector for Hive version based on Flink major version.

--- benchmarks/Flink/conf/setup_jars.sh
+++ benchmarks/Flink/conf/setup_jars.sh
@@ -8,9 +8,6 @@ export FLINK_OPT="$FLINK_HOME/opt"
 export FLINK_HADOOP_COMPATIBILITY_JAR="flink-hadoop-compatibility_${FLINK_SCALA_VERSION}-${FLINK_VERSION}.jar"
 export FLINK_HADOOP_COMPATIBILITY_PATH="${FLINK_LIB}/${FLINK_HADOOP_COMPATIBILITY_JAR}"
 export FLINK_HADOOP_COMPATIBILITY_URL="https://repo1.maven.org/maven2/org/apache/flink/flink-hadoop-compatibility_${FLINK_SCALA_VERSION}/${FLINK_VERSION}/${FLINK_HADOOP_COMPATIBILITY_JAR}"
-export FLINK_SQL_CONNECTOR_HIVE_JAR="flink-sql-connector-hive-${FLINK_HIVE_VERSION}_${FLINK_SCALA_VERSION}-${FLINK_VERSION}.jar"
-export FLINK_SQL_CONNECTOR_HIVE_PATH="${FLINK_LIB}/${FLINK_SQL_CONNECTOR_HIVE_JAR}"
-export FLINK_SQL_CONNECTOR_HIVE_URL="https://repo1.maven.org/maven2/org/apache/flink/flink-sql-connector-hive-${FLINK_HIVE_VERSION}_${FLINK_SCALA_VERSION}/${FLINK_VERSION}/${FLINK_SQL_CONNECTOR_HIVE_JAR}"
 export MAPREDUCE_JAR_FILE=$HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-client-core-*.jar
 
 if [ ! -f "$FLINK_HADOOP_COMPATIBILITY_PATH" ]; then
@@ -37,11 +34,39 @@ else
 fi
 
 if [ $GEN_AGGREGATION == "true" ] || [ $GEN_JOIN == "true" ] || [ $GEN_SCAN == "true" ]; then
-	# Remove the isolated loader from lib/ so it stops interfering
-	mv "$FLINK_LIB"/flink-table-planner-loader-*.jar "$FLINK_OPT"/ 2>/dev/null || true
-	# Copy the real planner
-	find "$FLINK_OPT" -maxdepth 1 -name "flink-table-planner*.jar" ! -name "*loader*" -exec cp {} "$FLINK_LIB/" \;
-
+	if [ $FLINK_SERIES == "1" ]; then
+		if [ $FLINK_MAJOR_VERSION != "1.20" ] &&
+			[ $FLINK_MAJOR_VERSION != "1.19" ] &&
+			[ $FLINK_MAJOR_VERSION != "1.18" ] &&
+			[ $FLINK_MAJOR_VERSION != "1.17" ] &&
+			[ $FLINK_MAJOR_VERSION != "1.16" ] &&
+			[ $FLINK_MAJOR_VERSION != "1.15" ]; then
+			m_exit "Flink version is not supported: $FLINK_VERSION"
+		fi
+	else
+        m_exit "Flink version is not supported: $FLINK_VERSION"
+	fi
+	
+	if [ $FLINK_MAJOR_VERSION == "1.15" ] || [ $FLINK_MAJOR_VERSION == "1.16" ] || [ $FLINK_MAJOR_VERSION == "1.17" ]; then
+		FLINK_HIVE_VERSION=3.1.2
+		# Remove the isolated loader from lib/ so it stops interfering
+		mv "$FLINK_LIB"/flink-table-planner-loader-*.jar "$FLINK_OPT"/ 2>/dev/null || true
+		# Copy the real planner
+		find "$FLINK_OPT" -maxdepth 1 -name "flink-table-planner*.jar" ! -name "*loader*" -exec cp {} "$FLINK_LIB/" \;
+	else
+		FLINK_HIVE_VERSION=3.1.3
+		# In 1.18+: The loader must be in lib/
+		if ls "$FLINK_OPT"/flink-table-planner-loader-*.jar 1>/dev/null 2>&1; then
+			mv "$FLINK_OPT"/flink-table-planner-loader-*.jar "$FLINK_LIB/"
+		fi
+		
+		find "$FLINK_LIB" -maxdepth 1 -name "flink-table-planner*.jar" ! -name "*loader*" -delete 2>/dev/null || true
+	fi
+	
+	FLINK_SQL_CONNECTOR_HIVE_JAR="flink-sql-connector-hive-${FLINK_HIVE_VERSION}_${FLINK_SCALA_VERSION}-${FLINK_VERSION}.jar"
+	FLINK_SQL_CONNECTOR_HIVE_PATH="${FLINK_LIB}/${FLINK_SQL_CONNECTOR_HIVE_JAR}"
+	FLINK_SQL_CONNECTOR_HIVE_URL="https://repo1.maven.org/maven2/org/apache/flink/flink-sql-connector-hive-${FLINK_HIVE_VERSION}_${FLINK_SCALA_VERSION}/${FLINK_VERSION}/${FLINK_SQL_CONNECTOR_HIVE_JAR}"
+	
 	if [ ! -f "$FLINK_SQL_CONNECTOR_HIVE_PATH" ]; then
     	m_echo "Flink SQL connector for Hive $FLINK_HIVE_VERSION not found: $FLINK_SQL_CONNECTOR_HIVE_PATH"
     	m_echo "Downloading $FLINK_SQL_CONNECTOR_HIVE_URL..."
@@ -66,7 +91,7 @@ if [ $GEN_AGGREGATION == "true" ] || [ $GEN_JOIN == "true" ] || [ $GEN_SCAN == "
 	if [ -d $HIVE_HOME ]; then
 		HIVE_FILTERED_CLASSPATH=$HADOOP_CLASSPATH
 		
-		for f in "$HIVE_LIB"/datanucleus-*.jar "$HIVE_LIB"/javax.jdo-*.jar "$HIVE_LIB"/derby-*.jar "$HIVE_LIB"//transaction-api-*.jar; do
+		for f in "$HIVE_LIB"/datanucleus-*.jar "$HIVE_LIB"/javax.jdo-*.jar "$HIVE_LIB"/derby-*.jar "$HIVE_LIB"/transaction-api-*.jar; do
     		if [ -f "$f" ]; then
         		HIVE_FILTERED_CLASSPATH="$HIVE_FILTERED_CLASSPATH:$f"
     		fi
