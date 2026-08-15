cfffe88eaaab59986ac94e3b79dd1bfa4457d5d8
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-15 13:46:40 +0200

    Add jar setup in env.sh
    Added setup for required jars in env.sh.

--- frameworks/src/Flink/etc/env.sh
+++ frameworks/src/Flink/etc/env.sh
@@ -75,6 +75,9 @@ else
 	cp -f $FLINK_CONFIG_SH_FILE $SOL_SBIN_DIR/config.sh
 fi
 
+# Setup required jars
+. ${SOL_BENCH_DIR}/conf/setup.jars
+
 add_conf_param "flink_conf_dir" $FLINK_CONF_DIR
 add_conf_param "flink_log_dir" $FLINK_LOG_DIR
 add_conf_param "flink_default_parallelism" $FLINK_PARALLELISM
