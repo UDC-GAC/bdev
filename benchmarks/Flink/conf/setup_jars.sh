bc5edc580a967d9939cfbfd0a23d347fcb45e1a7
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-15 13:41:47 +0200

    Remove flinkbench and TPCx-HS download logic
    Removed downloading logic for flinkbench and TPCx-HS JAR files.

--- benchmarks/Flink/conf/setup_jars.sh
+++ benchmarks/Flink/conf/setup_jars.sh
@@ -1,35 +1,16 @@
 #!/bin/sh
 
-export SORT_PARTITIONS=$FLINK_PARALLELISM
 # Hardcode last scala version supported by Flink 1.x
 # From Flink 2.x onwards, Flink is scala-free
 export FLINK_SCALA_VERSION=2.12
 export FLINK_LIB="$FLINK_HOME/lib"
 export FLINK_OPT="$FLINK_HOME/opt"
-export FLINK_BENCH_JAR_NAME=flinkbench-1.0_${FLINK_SCALA_VERSION}.jar
-export FLINK_BENCH_DIR=$SOL_BENCH_DIR/bin
-export FLINK_BENCH_JAR=$FLINK_BENCH_DIR/$FLINK_BENCH_JAR_NAME
 export FLINK_HADOOP_COMPATIBILITY_JAR="flink-hadoop-compatibility_${FLINK_SCALA_VERSION}-${FLINK_VERSION}.jar"
 export FLINK_HADOOP_COMPATIBILITY_PATH="${FLINK_LIB}/${FLINK_HADOOP_COMPATIBILITY_JAR}"
 export FLINK_HADOOP_COMPATIBILITY_URL="https://repo1.maven.org/maven2/org/apache/flink/flink-hadoop-compatibility_${FLINK_SCALA_VERSION}/${FLINK_VERSION}/${FLINK_HADOOP_COMPATIBILITY_JAR}"
 export FLINK_SQL_CONNECTOR_HIVE_JAR="flink-connector-hive_${FLINK_SCALA_VERSION}-${FLINK_VERSION}.jar"
 export FLINK_SQL_CONNECTOR_HIVE_PATH="${FLINK_LIB}/${FLINK_SQL_CONNECTOR_HIVE_JAR}"
 export FLINK_SQL_CONNECTOR_HIVE_URL="https://repo1.maven.org/maven2/org/apache/flink/flink-connector-hive_${FLINK_SCALA_VERSION}/${FLINK_VERSION}/${FLINK_SQL_CONNECTOR_HIVE_JAR}"
-
-if [ ! -f $FLINK_BENCH_JAR ]; then
-    # Download flinkbench jar file
-    URL=https://bdev.des.udc.es/dist/flinkbench
-    m_echo "Downloading $FLINK_BENCH_JAR_NAME"
-
-    wget -q -O $FLINK_BENCH_JAR $URL/$FLINK_BENCH_JAR_NAME
-
-    if [[ $? != 0 ]]; then
-		rm $FLINK_BENCH_JAR >& /dev/null
-        m_exit "Error when downloading $FLINK_BENCH_JAR_NAME"
-    fi
-else
-	m_echo "Using $FLINK_BENCH_JAR"
-fi
 
 if [ ! -f "$FLINK_HADOOP_COMPATIBILITY_PATH" ]; then
     m_echo "Flink Hadoop compatibility JAR not found: $FLINK_HADOOP_COMPATIBILITY_PATH"
@@ -46,30 +27,6 @@ if [ ! -f "$FLINK_HADOOP_COMPATIBILITY_PATH" ]; then
     	rm -f "$TMP_JAR"
         m_exit "Could not install $FLINK_HADOOP_COMPATIBILITY_JAR into $FLINK_LIB"
     fi
-fi
-
-if [ "$GEN_TPCX_HS" == "true" ]; then
-	if [ $FLINK_SERIES == "1" ]; then
-		export FLINK_TPCX_HS_JAR_NAME=tpcx-hs-flink-1.0_${FLINK_SCALA_VERSION}.jar
-		export TPCX_HS_JAR=$FLINK_BENCH_DIR/$FLINK_TPCX_HS_JAR_NAME
-	else
-        m_exit "Flink version is not supported: $FLINK_VERSION"
-	fi
-
-	if [ ! -f $TPCX_HS_JAR ]; then
-		# Download TPCx-HS jar file
-		URL=https://bdev.des.udc.es/dist/tpcx-hs
-		m_echo "Downloading $FLINK_TPCX_HS_JAR_NAME from $URL"
-
-    	wget -q -O $TPCX_HS_JAR $URL/$FLINK_TPCX_HS_JAR_NAME
-
-    	if [ $? != 0 ]; then
-			rm $TPCX_HS_JAR >& /dev/null
-			m_exit "Error when downloading $FLINK_TPCX_HS_JAR_NAME"
-    	fi
-	else
-		m_echo "Using $TPCX_HS_JAR"
-	fi
 fi
 
 if [ $GEN_AGGREGATION == "true" ] || [ $GEN_JOIN == "true" ] || [ $GEN_SCAN == "true" ]; then
