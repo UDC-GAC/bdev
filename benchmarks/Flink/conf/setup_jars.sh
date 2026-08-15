ea523ad120fed1dfb8b9cd5a67ab5b0fdbeea7de
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-15 16:54:52 +0200

    Define HIVE_LIB variable for Hive library path

--- benchmarks/Flink/conf/setup_jars.sh
+++ benchmarks/Flink/conf/setup_jars.sh
@@ -89,6 +89,7 @@ if [ $GEN_AGGREGATION == "true" ] || [ $GEN_JOIN == "true" ] || [ $GEN_SCAN == "
 	fi
 	
 	if [ -d $HIVE_HOME ]; then
+		$HIVE_LIB="${HIVE_HOME}/lib"
 		HIVE_FILTERED_CLASSPATH=$HADOOP_CLASSPATH
 		
 		for f in "$HIVE_LIB"/datanucleus-*.jar "$HIVE_LIB"/javax.jdo-*.jar "$HIVE_LIB"/derby-*.jar "$HIVE_LIB"/transaction-api-*.jar; do
