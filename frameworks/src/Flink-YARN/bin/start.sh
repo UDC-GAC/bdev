7993e964f5c7daa4d60288ee34faffd889a39ac1
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-15 18:55:07 +0200

    Enhance start.sh to include Hive setup
    Added setup for Hive jars and removed jar setup script.

--- frameworks/src/Flink-YARN/bin/start.sh
+++ frameworks/src/Flink-YARN/bin/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Setup required jars for Hive
+. ${SOL_BENCH_DIR}/conf/setup_hive.sh
+
 $COMMON_SRC_DIR/bin/start_hadoop_yarn.sh
 
 #Setup local temporary directories on all nodes
@@ -10,9 +13,6 @@ for j in `cat ${SLAVESFILE}`; do
  	$SSH_CMD $j "rm -rf ${FLINK_LOCAL_DIRS}/*"
         $SSH_CMD $j "mkdir -p ${FLINK_LOCAL_DIRS}"
 done
-
-# Setup required jars
-. ${SOL_BENCH_DIR}/conf/setup_jars.sh
 
 #Save Hadoop classpath to a file
 echo $HADOOP_CLASSPATH > $FLINK_HADOOP_CLASSPATH
