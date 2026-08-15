04c708f28d228b34e3968ba777fe7b399e457fa4
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-15 12:36:33 +0200

    Remove Hive issue resolution logic from configure.sh
    Removed the resolve_hive_issues function and related Hive setup logic.

--- benchmarks/Hadoop/conf/configure.sh
+++ benchmarks/Hadoop/conf/configure.sh
@@ -1,43 +1,5 @@
 #!/bin/bash
 
-function resolve_hive_issues() {
-    local HIVE_LIB="${HIVE_HOME}/lib"
-    local HADOOP_LIB="${HADOOP_HOME}/share/hadoop/common/lib"
-    local BACKUP_FILE=$(ls ${HIVE_LIB}/guava-*-original.bak 2>/dev/null | head -n 1)
-	local COMMONS_COLLECTIONS_JAR=commons-collections-3.2.2.jar
-	
-    if [[ -z "${BACKUP_FILE}" ]]; then
-        local ORIGINAL_JAR=$(ls ${HIVE_LIB}/guava-*.jar 2>/dev/null | grep -v 'original.bak' | head -n 1)
-        
-        if [[ -z "${ORIGINAL_JAR}" ]]; then
-            m_exit "No guava.jar was found in $HIVE_LIB"
-        fi
-        
-        mv "${ORIGINAL_JAR}" "${ORIGINAL_JAR}-original.bak"
-        BACKUP_FILE="${ORIGINAL_JAR}-original.bak"
-    fi
-
-    rm -f ${HIVE_LIB}/guava-*.jar
-
-    if [[ "${HADOOP_SERIES}" == 3 ]]; then
-        cp ${HADOOP_LIB}/guava-*.jar ${HIVE_LIB}/
-    else
-        local RESTORED_JAR="${BACKUP_FILE%-original.bak}"
-        cp "${BACKUP_FILE}" "${RESTORED_JAR}"
-    fi
-
-	if [[ ! -f ${HIVE_LIB}/$COMMONS_COLLECTIONS_JAR ]]; then
-		URL=https://bdev.des.udc.es/dist
-		m_echo "Downloading $COMMONS_COLLECTIONS_JAR from $URL"
-		wget -q -O ${HIVE_LIB}/$COMMONS_COLLECTIONS_JAR $URL/$COMMONS_COLLECTIONS_JAR
-
-		if [ $? != 0 ]; then
-			rm ${HIVE_LIB}/$COMMONS_COLLECTIONS_JAR >& /dev/null
-			m_exit "Error when downloading $COMMONS_COLLECTIONS_JAR"
-    	fi
-	fi
-}
-
 export PEGASUS_JAR=$THIRD_PARTY_DIR/pegasus-2.0/pegasus-2.0.jar
 
 if [[ ( $GEN_KMEANS == "true" || $GEN_BAYES == "true" ) && ! -d $MAHOUT_HOME ]]; then
@@ -69,36 +31,6 @@ if [[ ( $GEN_KMEANS == "true" || $GEN_BAYES == "true" ) && ! -d $MAHOUT_HOME ]];
 	tar -xzf $TMP_MAHOUT_FILE -C $THIRD_PARTY_DIR
 	mv $TMP_MAHOUT_DIR $MAHOUT_HOME
 	rm $TMP_MAHOUT_FILE
-fi
-
-if [[ ( $GEN_AGGREGATION == "true" || $GEN_JOIN == "true" || $GEN_SCAN == "true" ) ]]; then
-	if [[ "${HIVE_VERSION}" == 4.* ]]; then
-		m_exit "Hive 4.x is not supported: $HIVE_VERSION"
-	fi
-	
-	if [[ ! -d $HIVE_HOME ]]; then
-		URL="https://archive.apache.org/dist/hive/"
-		TMP_HIVE_FILE=$THIRD_PARTY_DIR/apache-hive-${HIVE_VERSION}-bin.tar.gz
-		TMP_HIVE_DIR=$THIRD_PARTY_DIR/apache-hive-${HIVE_VERSION}-bin
-
-		m_echo "Downloading hive-$HIVE_VERSION"
-		wget -q -O $TMP_HIVE_FILE $URL/hive-$HIVE_VERSION/apache-hive-${HIVE_VERSION}-bin.tar.gz
-
-		if [[ $? != 0 ]]; then
-			rm $TMP_HIVE_FILE
-			TMP_HIVE_FILE=$THIRD_PARTY_DIR/hive-${HIVE_VERSION}-bin.tar.gz
-			TMP_HIVE_DIR=$THIRD_PARTY_DIR/hive-${HIVE_VERSION}-bin
-			wget -q -O $TMP_HIVE_FILE $URL/hive-$HIVE_VERSION/hive-${HIVE_VERSION}-bin.tar.gz
-		fi
-
-		m_echo "Extracting $TMP_HIVE_FILE"
-		tar -xzf $TMP_HIVE_FILE -C $THIRD_PARTY_DIR
-		mv $TMP_HIVE_DIR $HIVE_HOME
-		rm $TMP_HIVE_FILE
-	fi
-
-	# Manage Hive issues (Guava, commons-collections)
-	resolve_hive_issues
 fi
 
 if [[ "$GEN_TPCX_HS" == "true" ]]; then
