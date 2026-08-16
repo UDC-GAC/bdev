8bcc5e5bda3a0024f3583c8f42b8f8bcc7b2b557
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-17 01:12:04 +0200

    Update start.sh to source setup_jars.sh
    Changed script to source setup_jars.sh instead of setup_hive.sh.

--- frameworks/src/Flink-YARN/bin/start.sh
+++ frameworks/src/Flink-YARN/bin/start.sh
@@ -11,8 +11,8 @@ for j in `cat ${SLAVESFILE}`; do
         $SSH_CMD $j "mkdir -p ${FLINK_LOCAL_DIRS}"
 done
 
-# Setup required jars for Hive
-. ${SOL_BENCH_DIR}/conf/setup_hive.sh
+# Setup required jars
+. ${SOL_BENCH_DIR}/conf/setup_jars.sh
 
 # Save Hadoop classpath to a file
 echo $HADOOP_CLASSPATH > $FLINK_HADOOP_CLASSPATH
