a8d1b1dc7a4997cf837e50ad8c3456e78e0aa1a8
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-17 00:46:20 +0200

    Refactor Hive setup script for version handling

--- benchmarks/Flink/conf/setup_hive.sh
+++ benchmarks/Flink/conf/setup_hive.sh
@@ -6,6 +6,20 @@ export FLINK_SCALA_VERSION=2.12
 export FLINK_LIB="$FLINK_HOME/lib"
 export FLINK_OPT="$FLINK_HOME/opt"
 
+if [ $FLINK_MAJOR_VERSION == "1.15" ] || [ $FLINK_MAJOR_VERSION == "1.16" ]; then
+	export FLINK_HIVE_VERSION=3.1.2
+else
+	export FLINK_HIVE_VERSION=3.1.3
+fi
+
+FLINK_SQL_CONNECTOR_HIVE_JAR="flink-connector-hive_${FLINK_SCALA_VERSION}-${FLINK_VERSION}.jar"
+FLINK_SQL_CONNECTOR_HIVE_PATH="${FLINK_LIB}/${FLINK_SQL_CONNECTOR_HIVE_JAR}"
+FLINK_SQL_CONNECTOR_HIVE_URL="https://repo1.maven.org/maven2/org/apache/flink/flink-connector-hive_${FLINK_SCALA_VERSION}/${FLINK_VERSION}/${FLINK_SQL_CONNECTOR_HIVE_JAR}"
+HIVE_EXEC_CORE_JAR=hive-exec-${FLINK_HIVE_VERSION}-core.jar
+HIVE_EXEC_CORE_PATH="${FLINK_LIB}/${HIVE_EXEC_CORE_JAR}"
+HIVE_EXEC_CORE_URL="https://repo1.maven.org/maven2/org/apache/hive/hive-exec/${FLINK_HIVE_VERSION}/${HIVE_EXEC_CORE_JAR}"
+HIVE_FILTERED_CLASSPATH=$HADOOP_CLASSPATH
+	
 if [ $GEN_AGGREGATION == "true" ] || [ $GEN_JOIN == "true" ] || [ $GEN_SCAN == "true" ]; then
 	if [ -z $HIVE_HOME ]; then
 		m_exit "HIVE_HOME is not defined or is empty"
@@ -16,22 +30,10 @@ if [ $GEN_AGGREGATION == "true" ] || [ $GEN_JOIN == "true" ] || [ $GEN_SCAN == "
 	else
 		m_exit "HIVE_HOME does not exist or is not a directory: $HIVE_HOME"
 	fi
-		
-	if [ $FLINK_MAJOR_VERSION == "1.15" ] || [ $FLINK_MAJOR_VERSION == "1.16" ]; then
-		export FLINK_HIVE_VERSION=3.1.2
-	else
-		export FLINK_HIVE_VERSION=3.1.3
-	fi
 
-	FLINK_SQL_CONNECTOR_HIVE_JAR="flink-connector-hive_${FLINK_SCALA_VERSION}-${FLINK_VERSION}.jar"
-	FLINK_SQL_CONNECTOR_HIVE_PATH="${FLINK_LIB}/${FLINK_SQL_CONNECTOR_HIVE_JAR}"
-	FLINK_SQL_CONNECTOR_HIVE_URL="https://repo1.maven.org/maven2/org/apache/flink/flink-connector-hive_${FLINK_SCALA_VERSION}/${FLINK_VERSION}/${FLINK_SQL_CONNECTOR_HIVE_JAR}"
-	HIVE_EXEC_CORE_JAR=hive-exec-${FLINK_HIVE_VERSION}-core.jar
-	HIVE_EXEC_CORE_PATH="${FLINK_LIB}/${HIVE_EXEC_CORE_JAR}"
-	HIVE_EXEC_CORE_URL="https://repo1.maven.org/maven2/org/apache/hive/hive-exec/${FLINK_HIVE_VERSION}/${HIVE_EXEC_CORE_JAR}"
-	HIVE_FILTERED_CLASSPATH=$HADOOP_CLASSPATH
-
-	if [ ! -f "$HIVE_EXEC_CORE_PATH" ]; then
+	if [ -f "${FLINK_OPT}/${HIVE_EXEC_CORE_JAR}" ]; then
+        mv "${FLINK_OPT}/${HIVE_EXEC_CORE_JAR}" "$FLINK_LIB/"	
+	elif [ ! -f "$HIVE_EXEC_CORE_PATH" ]; then
 	    m_echo "Hive exec core jar not found: $HIVE_EXEC_CORE_PATH"
     	m_echo "Downloading $HIVE_EXEC_CORE_URL..."
 
@@ -47,8 +49,10 @@ if [ $GEN_AGGREGATION == "true" ] || [ $GEN_JOIN == "true" ] || [ $GEN_SCAN == "
         	m_exit "Could not install $HIVE_EXEC_CORE_JAR into $FLINK_LIB"
     	fi
 	fi
-	
-	if [ ! -f "$FLINK_SQL_CONNECTOR_HIVE_PATH" ]; then
+
+	if [ -f "${FLINK_OPT}/${FLINK_SQL_CONNECTOR_HIVE_JAR}" ]; then
+        mv "${FLINK_OPT}/${FLINK_SQL_CONNECTOR_HIVE_JAR}" "$FLINK_LIB/"
+	elif [ ! -f "$FLINK_SQL_CONNECTOR_HIVE_PATH" ]; then
     	m_echo "Flink SQL connector for Hive not found: $FLINK_SQL_CONNECTOR_HIVE_PATH"
     	m_echo "Downloading $FLINK_SQL_CONNECTOR_HIVE_URL..."
 
@@ -84,4 +88,12 @@ if [ $GEN_AGGREGATION == "true" ] || [ $GEN_JOIN == "true" ] || [ $GEN_SCAN == "
         done
 
 	export HADOOP_CLASSPATH=$HIVE_FILTERED_CLASSPATH
+else
+    if [ -f "$FLINK_SQL_CONNECTOR_HIVE_PATH" ]; then
+        mv "$FLINK_SQL_CONNECTOR_HIVE_PATH" "$FLINK_OPT/"
+    fi
+	
+	if [ -f "$HIVE_EXEC_CORE_PATH" ]; then
+        mv "$HIVE_EXEC_CORE_PATH" "$FLINK_OPT/"
+    fi
 fi
