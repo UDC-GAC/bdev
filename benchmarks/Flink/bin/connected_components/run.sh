484444f8d5c5f2db77a0cb41b32ff9ef6726cb29
Roberto R. Exposito <rreye@udc.es>
2026-08-17 02:00:08 +0200

    Avoid using deprecated Gelly

--- benchmarks/Flink/bin/connected_components/run.sh
+++ benchmarks/Flink/bin/connected_components/run.sh
@@ -4,5 +4,5 @@ storage_rm -R ${OUTPUT_CC}
 
 run_benchmark "$FLINK_HOME/bin/flink \
 	${DEPLOY_ARGS} \
-	--class es.udc.gac.flinkbench.dataset.ScalaGellyConnectedComponents $FLINK_BENCH_JAR \
+	--class es.udc.gac.flinkbench.dataset.ScalaConnectedComponents $FLINK_BENCH_JAR \
 	${INPUT_CC}/edges ${OUTPUT_CC} ${CC_PAGES} ${CC_MAX_ITERATIONS}"
