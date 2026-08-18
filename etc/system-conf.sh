f1a77960a7946fef04fd6caf72f07cf7cd9b935e
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-18 14:37:49 +0200

    Add RDMA interface to system configuration
    Added RDMA interface for InfiniBand/RoCE to configuration.

--- etc/system-conf.sh
+++ etc/system-conf.sh
@@ -5,8 +5,9 @@
 export TMP_DIR=/tmp/$USER/$APP_NAME	# Directory to store temporary files
 export LOCAL_DIRS=$TMP_DIR 	# Comma-separated list of directories to store local data in each node
 export LOOPBACK_IP=127.0.0.1	# IP of the loopback network interface
-export ETH_INTERFACE=eth0 	# Ethernet interface to use in the nodes
-export IPOIB_INTERFACE=ib0 	# IPoIB interface to use in the nodes
+export ETH_INTERFACE=eth0 	# Ethernet network interface for TCP/IP 
+export IPOIB_INTERFACE=ib0 	# Network interface for IP over InfiniBand (IPoIB)
+export RDMA_INTERFACE=mlx5_0  # RDMA interface for InfiniBand/RoCE
 export CPUS_PER_NODE=`grep "^physical id" /proc/cpuinfo | sort -u | wc -l`	# CPUs per node
 export CORES_PER_CPU=`grep "^core id" /proc/cpuinfo | sort -u | wc -l`	# Cores per CPU
 export CORES_PER_NODE=$(( $CPUS_PER_NODE * $CORES_PER_CPU ))	# Cores per node
