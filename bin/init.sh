310a797d86b9adab5c215dbda959d58307c40ea4
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-08-18 12:12:25 +0200

    Remove storage backend functions loading from init.sh
    Removed loading of storage backend functions from init script.

--- bin/init.sh
+++ bin/init.sh
@@ -7,9 +7,6 @@ m_echo "Initializing"
 # Load nodes and IPs
 . $BDEV_BIN_DIR/load-nodes.sh
 
-# Load storage backend functions
-. $COMMON_SRC_DIR/lib/storage_backend.sh
-
 . $CLEAN_DAEMONS_SCRIPT
 
 begin_report
