45eead1e52d5e523a66bc6b2edaa8a26520892ef
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2026-09-01 13:12:19 +0200

    Avoid warnings when using egrep and other improvements

--- bin/clean-node-daemons.sh
+++ bin/clean-node-daemons.sh
@@ -2,22 +2,21 @@
 
 DAEMONS="NameNode|SecondaryNameNode|DataNode|ResourceManager|NodeManager|JobHistoryServer|ApplicationHistoryServer|RunJar|Child|MRAppMaster|YarnChild|SparkSubmit|CoarseGrainedExecutorBackend|ApplicationMaster|Master|HistoryServer|Worker|ExecutorLauncher|JobManager|TaskManager|StandaloneSessionClusterEntrypoint|TaskManagerRunner|CliFrontend"
 
-DAEMON_PIDS=`${JPS} | egrep $DAEMONS | awk '{print $1}'`
-DAEMON_NAMES=`${JPS} | egrep $DAEMONS | awk '{print $2}'`
-	
-if [[ \"x$DAEMON_PIDS\" != \"x\" ]]; then
-	DAEMON_PIDS=`echo $DAEMON_PIDS`
-	DAEMON_NAMES=`echo $DAEMON_NAMES`
-	echo "$HOSTNAME: cleaning up $DAEMON_NAMES with PIDs $DAEMON_PIDS"
-	kill -9 $DAEMON_PIDS
+JPS_MATCHES=$(${JPS} 2>/dev/null | grep -E "${DAEMONS}")
+
+if [[ -n "$JPS_MATCHES" ]]; then
+    DAEMON_PIDS=$(echo "$JPS_MATCHES" | awk '{print $1}')
+    DAEMON_NAMES=$(echo "$JPS_MATCHES" | awk '{print $2}')
+    echo "$HOSTNAME: cleaning up $DAEMON_NAMES with PIDs $DAEMON_PIDS"
+    kill -9 $DAEMON_PIDS 2>/dev/null
 fi
 
-DOOL_PID=`ps -elf | grep ${PYTHON_BIN} | grep ${DOOL_COMMAND_NAME} | grep -v "export" | awk '{print $4}'`
+DOOL_PID=$(ps -elf | grep "${PYTHON_BIN}" | grep "${DOOL_COMMAND_NAME}" | grep -v "export" | grep -v "grep" | awk '{print $4}')
 
-if [[ \"x$DOOL_PID\" != \"x\" ]]; then
-	DOOL_PID=`echo $DOOL_PID`
-	echo "$HOSTNAME: cleaning up ${DOOL_COMMAND_NAME} with PID $DOOL_PID"
-	kill -9 $DOOL_PID
+if [[ -n "$DOOL_PID" ]]; then
+    DOOL_PID=$(echo $DOOL_PID)
+    echo "$HOSTNAME: cleaning up ${DOOL_COMMAND_NAME} with PID $DOOL_PID"
+    kill -9 $DOOL_PID 2>/dev/null
 fi
 
 killall -u $USER -q -9 ocount
