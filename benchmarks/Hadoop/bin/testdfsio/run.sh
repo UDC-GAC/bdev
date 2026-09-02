1aefc99d347504c3b470f26bd0500a358da93bc5
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2026-09-02 18:28:07 +0200

    Fix bug when running testdfsio

--- benchmarks/Hadoop/bin/testdfsio/run.sh
+++ benchmarks/Hadoop/bin/testdfsio/run.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 TESTDFSIO="${HADOOP_EXECUTABLE} org.apache.hadoop.fs.TestDFSIO"
-COMMAND="$TESTDFSIO -write -nrFiles $DFSIO_N_FILES -fileSize $DFSIO_FILE_SIZE ;
-$TESTDFSIO -read -nrFiles $DFSIO_N_FILES -fileSize $DFSIO_FILE_SIZE"
+WRITE_TEST="$TESTDFSIO -write -nrFiles $DFSIO_N_FILES -fileSize $DFSIO_FILE_SIZE"
+READ_TEST="$TESTDFSIO -read -nrFiles $DFSIO_N_FILES -fileSize $DFSIO_FILE_SIZE"
+WRITE_THROUGHPUT=""
+READ_THROUGHPUT=""
 
-run_benchmark "$COMMAND"
+run_benchmark "$WRITE_TEST"
 
 if [[ "$ELAPSED_TIME" != "TIMEOUT" && "$ELAPSED_TIME" != "FAILED" ]]; then
 	WRITE_THROUGHPUT=$(grep -A 8 "TestDFSIO ----- : write" "$TMPLOGFILE" | awk '/Throughput mb\/sec:/ {print $NF}')
+fi
+
+run_benchmark "$READ_TEST"
+
+if [[ "$ELAPSED_TIME" != "TIMEOUT" && "$ELAPSED_TIME" != "FAILED" ]]; then
 	READ_THROUGHPUT=$(grep -A 8 "TestDFSIO ----- : read" "$TMPLOGFILE" | awk '/Throughput mb\/sec:/ {print $NF}')
+
+fi
+
+if [[ -n "$WRITE_THROUGHPUT" && -n "$READ_THROUGHPUT" ]]; then
 	m_echo "Write/Read throughput (MB/s): $WRITE_THROUGHPUT/$READ_THROUGHPUT"
 fi
