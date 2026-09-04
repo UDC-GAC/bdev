7a38098ec465e3382057e98db0313fc64025cf45
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-04 22:22:12 +0200

    Refactor PID check in kill_stat_monitor.sh

--- bin/stat/kill_stat_monitor.sh
+++ bin/stat/kill_stat_monitor.sh
@@ -2,8 +2,7 @@
 
 DOOL_PID=$(ps -elf | grep ${PYTHON_BIN} | grep ${DOOL_COMMAND_NAME} | grep -v "ssh" | grep -v "export" | awk '{print $4}')
 
-if [[ \"x$DOOL_PID\" != \"x\" ]]; then
-	DOOL_PID=`echo $DOOL_PID`
+if [[ -n "$DOOL_PID" ]]; then
 	echo "$HOSTNAME: cleaning up ${DOOL_COMMAND_NAME} with PID $DOOL_PID"
 	kill -9 $DOOL_PID
 fi
