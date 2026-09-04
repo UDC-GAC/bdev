036de0cf7951123edd8a16ae83bd844b2dee18b6
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2026-09-04 17:06:31 +0200

    Optimize script

--- bin/cleanup/process.sh
+++ bin/cleanup/process.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-DAEMONS="NameNode|SecondaryNameNode|DataNode|ResourceManager|NodeManager|JobHistoryServer|ApplicationHistoryServer|RunJar|Child|MRAppMaster|YarnChild|SparkSubmit|CoarseGrainedExecutorBackend|ApplicationMaster|Master|HistoryServer|Worker|ExecutorLauncher|JobManager|TaskManager|StandaloneSessionClusterEntrypoint|TaskManagerRunner|CliFrontend"
+PROCESS="NameNode|SecondaryNameNode|DataNode|ResourceManager|NodeManager|JobHistoryServer|ApplicationHistoryServer|RunJar|Child|MRAppMaster|YarnChild|SparkSubmit|CoarseGrainedExecutorBackend|ApplicationMaster|Master|HistoryServer|Worker|ExecutorLauncher|JobManager|TaskManager|StandaloneSessionClusterEntrypoint|TaskManagerRunner|CliFrontend"
 
-JPS_MATCHES=$(${JPS} 2>/dev/null | grep -E "${DAEMONS}")
+JPS_MATCHES=$("$JPS" 2>/dev/null | grep -E "$PROCESS")
 
 if [[ -n "$JPS_MATCHES" ]]; then
-    DAEMON_PIDS=$(echo "$JPS_MATCHES" | awk '{print $1}')
-    DAEMON_NAMES=$(echo "$JPS_MATCHES" | awk '{print $2}')
-    echo "$HOSTNAME: cleaning up $DAEMON_NAMES with PIDs $DAEMON_PIDS"
-    kill -9 $DAEMON_PIDS 2>/dev/null
+    PROCESS_PIDS=$(awk '{print $1}' <<< "$JPS_MATCHES")
+    PROCESS_NAMES=$(awk '{print $2}' <<< "$JPS_MATCHES")
+    echo "$HOSTNAME: cleaning up $PROCESS_NAMES with PIDs $PROCESS_PIDS"
+    kill -9 $PROCESS_PIDS 2>/dev/null
 fi
 
-DOOL_PID=$(ps -elf | grep "${PYTHON_BIN}" | grep "${DOOL_COMMAND_NAME}" | grep -v "export" | grep -v "grep" | awk '{print $4}')
+DOOL_PID=$(ps -elf | grep "$PYTHON_BIN" | grep "$DOOL_COMMAND_NAME" | grep -v "export" | grep -v "grep" | awk '{print $4}')
 
 if [[ -n "$DOOL_PID" ]]; then
-    DOOL_PID=$(echo $DOOL_PID)
     echo "$HOSTNAME: cleaning up ${DOOL_COMMAND_NAME} with PID $DOOL_PID"
     kill -9 $DOOL_PID 2>/dev/null
 fi
 
-if [[ $ENABLE_OPROFILE == "true" ]]; then
-	killall -u $USER -q -9 $OPROFILE_BIN
+if [[ "$ENABLE_OPROFILE" == "true" ]]; then
+	killall -u "$USER" -q -9 "$OPROFILE_BIN" 2>/dev/null
 fi
 
-if [[ $ENABLE_RAPL == "true" ]]; then
-	killall -u $USER -q -9 rapl_plot
+if [[ "$ENABLE_RAPL" == "true" ]]; then
+	killall -u "$USER" -q -9 rapl_plot 2>/dev/null
 fi
