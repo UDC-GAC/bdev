b64e651a64b310d801f46e2c89975bcb28191f67
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-05 19:30:00 +0200

    Cleanup env.sh by removing unused variables and logic
    Removed unused environment variables related to daemons and sbin directories. Cleaned up the script by eliminating the config.sh file checks and copying logic.

--- frameworks/src/Flink-YARN/etc/env.sh
+++ frameworks/src/Flink-YARN/etc/env.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export SOL_BENCH_DIR=$BENCHMARKS_DIR/Flink
 export SOL_TEMPLATE_DIR=$TEMPLATES_DIR/Flink
-export SOL_DAEMONS_DIR=$DAEMONS_DIR/Flink
-export SOL_SBIN_DIR=$SOLUTION_HOME/bin
 export SOL_CONF_DIR_SRC=$SOLUTION_HOME/conf
 export SOL_CONF_DIR=$SOLUTION_REPORT_DIR/conf/flink
 export SOL_LOG_DIR=$SOLUTION_REPORT_DIR/logs/flink
@@ -16,7 +14,6 @@ export FLINK_VERSION=${SOLUTION_HOME##*/}
 export FLINK_MAJOR_VERSION=${FLINK_VERSION%.*}
 export FLINK_SERIES=${FLINK_VERSION%%.*}
 export FLINK_CONF_DIR=$SOL_CONF_DIR
-export FLINK_SBIN_DIR=$SOL_SBIN_DIR
 export FLINK_LOG_DIR=$SOL_LOG_DIR
 export PATH=$FLINK_HOME/bin:$PATH
 export FLINK_TASKMANAGERS=$(($FLINK_TASKMANAGERS_PER_NODE * $WORKERS_NUMBER))
@@ -53,16 +50,6 @@ export DEPLOY_ARGS="run -t yarn-session"
 export YARN_PROPS_FILE_CUSTOM="$TMP_DIR/.yarn-properties-$USER"
 export YARN_PROPS_FILE_DEFAULT="/tmp/.yarn-properties-$USER"
 export YARN_APP_ID=""
-
-# Set and copy config.sh file according to Flink version
-FLINK_CONFIG_SH_FILE=$SOL_DAEMONS_DIR/config/config-${FLINK_MAJOR_VERSION}.sh
-
-if [ ! -f $FLINK_CONFIG_SH_FILE ]; then
-        m_exit "Flink config.sh file not found: $FLINK_CONFIG_SH_FILE"
-else
-	m_echo "Copying Flink config.sh file: $FLINK_CONFIG_SH_FILE"
-	cp -f $FLINK_CONFIG_SH_FILE $SOL_SBIN_DIR/config.sh
-fi
 
 add_conf_param "flink_conf_dir" $FLINK_CONF_DIR
 add_conf_param "flink_log_dir" $FLINK_LOG_DIR
