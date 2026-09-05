142f791ed16cde5ca85a3182d84766a7a4b7a068
Roberto R. Exposito <rreye@udc.es>
2026-09-05 19:30:56 +0200

    Remove unused scripts

--- bin/copy-daemons.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ ! -z $SOL_DAEMONS_DIR ]]; then
-	m_echo "NO Copying daemons from $SOL_DAEMONS_DIR"
-	#cp $SOL_DAEMONS_DIR/* $SOL_SBIN_DIR 2> /dev/null
-else
-	m_echo "No daemons directory to copy"
-fi
