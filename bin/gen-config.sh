f391ff18a5cfb7d36e5c8fb5110927011d84be77
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-06 19:07:33 +0200

    Update echo message for configuration generation

--- bin/gen-config.sh
+++ bin/gen-config.sh
@@ -35,7 +35,7 @@ generate_framework_config() {
     [[ ! -d "$src_dir" ]] && m_exit "Source conf dir does not exist: $src_dir"
     [[ ! -d "$template_dir" ]] && m_exit "Template dir does not exist: $template_dir"
 
-	m_echo "Generating configuration for $src_dir using templates from $template_dir"
+	m_echo "Generating configuration files: $src_dir"
 
     # Copy base configuration from tarball
 	if ! mkdir -p "$target_dir"; then
