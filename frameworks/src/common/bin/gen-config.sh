62b12acf66243ef8d40ea4be636d6ff1c811436f
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-06 23:22:54 +0200

    Use WORKERSFILE variable in gen-config.sh

--- frameworks/src/common/bin/gen-config.sh
+++ frameworks/src/common/bin/gen-config.sh
@@ -2,9 +2,11 @@
 
 . "$BDEV_BIN_DIR/gen-config.sh"
 
+export WORKERSFILE=$HADOOP_WORKERSFILE
+
 generate_framework_config \
   "$HADOOP_CONF_DIR_SRC" \
   "$HADOOP_TEMPLATE_DIR" \
   "$HADOOP_CONF_DIR" \
   "$HADOOP_LOG_DIR" \
-  "$HADOOP_WORKERSFILE"
+  "$WORKERSFILE"
