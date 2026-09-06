e009f68793d2d36788ab6b1b87047b1bc3aaf229
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-06 13:06:55 +0200

    Fix conditional check for SPARK_HISTORY_SERVER

--- frameworks/src/Spark/bin/start.sh
+++ frameworks/src/Spark/bin/start.sh
@@ -6,7 +6,7 @@ m_echo "Starting the standalone Spark cluster"
 "$SPARK_HOME/sbin/start-master.sh"
 bash "$SOLUTION_DIR/bin/start-workers.sh"
 
-if [ $SPARK_HISTORY_SERVER == "true" ]; then
+if [[ "$SPARK_HISTORY_SERVER" == "true" ]]; then
 	storage_mkdir "$SPARK_HISTORY_SERVER_DIR"
 	storage_chmod -R 777 "$SPARK_HISTORY_SERVER_DIR"
 
