b01cf0c68956bc9a86ac61f36f0fc275ded219a7
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-06 18:17:40 +0200

    Remove GEN_CONFIG_SCRIPT from env.sh
    Removed GEN_CONFIG_SCRIPT variable assignment.

--- frameworks/src/Flink/etc/env.sh
+++ frameworks/src/Flink/etc/env.sh
@@ -4,7 +4,6 @@ export SOL_TEMPLATE_DIR=$TEMPLATES_DIR/Flink
 export SOL_CONF_DIR_SRC=$SOLUTION_HOME/conf
 export SOL_CONF_DIR=$SOLUTION_REPORT_DIR/conf/flink
 export SOL_LOG_DIR=$SOLUTION_REPORT_DIR/logs/flink
-export GEN_CONFIG_SCRIPT=$SOLUTION_DIR/bin/gen-config.sh
 export MASTERFILE=$SOL_CONF_DIR/masters
 export WORKERSFILE=$SOL_CONF_DIR/workers
 
