cc50c822b0f9b60ad5964b55ecd82e55da2b96f9
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-06 18:59:54 +0200

    Conditionally source gen-config.sh for HDFS

--- frameworks/src/Spark/bin/gen-config.sh
+++ frameworks/src/Spark/bin/gen-config.sh
@@ -12,4 +12,6 @@ generate_framework_config \
     "$WORKERSFILE"
 
 # Hadoop
-. "$COMMON_SRC_DIR/bin/gen-config.sh"
+if [[ "${STORAGE_BACKEND,,}" == "hdfs" ]]; then
+    . "$COMMON_SRC_DIR/bin/gen-config.sh"
+fi
