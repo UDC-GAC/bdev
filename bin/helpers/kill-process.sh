c73b570d66930d41b128e2e6239e834c6f480545
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-06 19:51:37 +0200

    Remove set -eo pipefail from kill-process.sh

--- bin/helpers/kill-process.sh
+++ bin/helpers/kill-process.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -eo pipefail
 
 PROCESS="NameNode|SecondaryNameNode|DataNode|ResourceManager|NodeManager|JobHistoryServer|ApplicationHistoryServer|RunJar|Child|MRAppMaster|YarnChild|SparkSubmit|CoarseGrainedExecutorBackend|ApplicationMaster|Master|HistoryServer|Worker|ExecutorLauncher|JobManager|TaskManager|StandaloneSessionClusterEntrypoint|TaskManagerRunner|CliFrontend"
 
