3d0e1a0dd824a378cc0b9c8966baf3d4da99e13c
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-06 18:17:26 +0200

    Remove GEN_CONFIG_SCRIPT from environment variables
    Removed GEN_CONFIG_SCRIPT variable from env.sh

--- frameworks/src/Spark/etc/env.sh
+++ frameworks/src/Spark/etc/env.sh
@@ -4,7 +4,6 @@ export SOL_TEMPLATE_DIR=$TEMPLATES_DIR/Spark
 export SOL_CONF_DIR_SRC=$SOLUTION_HOME/conf
 export SOL_CONF_DIR=$SOLUTION_REPORT_DIR/conf/spark
 export SOL_LOG_DIR=$SOLUTION_REPORT_DIR/logs/spark
-export GEN_CONFIG_SCRIPT=$SOLUTION_DIR/bin/gen-config.sh
 export MASTERFILE=$SOL_CONF_DIR/masters
 export WORKERSFILE=$SOL_CONF_DIR/workers
 
