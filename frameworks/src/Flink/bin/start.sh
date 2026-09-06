a8c47af0c90b0ea8785a458bc269e5cd1c23de38
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-06 13:13:40 +0200

    Fix script syntax and update Flink startup commands

--- frameworks/src/Flink/bin/start.sh
+++ frameworks/src/Flink/bin/start.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-$COMMON_SRC_DIR/bin/start_hadoop_yarn.sh
+"$COMMON_SRC_DIR/bin/start_hadoop_yarn.sh"
 
 # Setup required jars
-. ${SOL_BENCH_DIR}/conf/setup_jars.sh
-
-#Save Hadoop classpath to a file
-echo $HADOOP_CLASSPATH > $FLINK_HADOOP_CLASSPATH
+. "$SOL_BENCH_DIR/conf/setup_jars.sh"
 
 # Create LOG dir
 mkdir -p "$FLINK_LOG_DIR" 2>/dev/null || true
 
 m_echo "Starting the standalone Flink cluster (Session Mode)"
-$FLINK_HOME/bin/start-cluster.sh
+"$FLINK_BIN_DIR/jobmanager.sh" start
+bash "$SOLUTION_DIR/bin/start-workers.sh"
 
-if [ $FLINK_HISTORY_SERVER == "true" ]; then
-    storage_mkdir ${FLINK_HISTORY_SERVER_DIR}
-	storage_chmod -R 777 ${FLINK_HISTORY_SERVER_DIR}
+if [[ "$FLINK_HISTORY_SERVER" == "true" ]]; then
+    storage_mkdir "$FLINK_HISTORY_SERVER_DIR"
+	storage_chmod -R 777 "$FLINK_HISTORY_SERVER_DIR"
 
     #Flink history server
-    $FLINK_HOME/bin/historyserver.sh start
+    "$FLINK_HOME/bin/historyserver.sh" start
 fi
