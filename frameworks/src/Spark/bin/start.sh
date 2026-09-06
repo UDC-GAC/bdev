e3dc0d9ebd6248c51b9bdf69e5c2a5b501df5663
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-06 13:06:25 +0200

    Fix quoting in start.sh script

--- frameworks/src/Spark/bin/start.sh
+++ frameworks/src/Spark/bin/start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-$COMMON_SRC_DIR/bin/start_hadoop_yarn.sh
+"$COMMON_SRC_DIR/bin/start_hadoop_yarn.sh"
 
 m_echo "Starting the standalone Spark cluster"
-$SPARK_HOME/sbin/start-master.sh
+"$SPARK_HOME/sbin/start-master.sh"
 bash "$SOLUTION_DIR/bin/start-workers.sh"
 
 if [ $SPARK_HISTORY_SERVER == "true" ]; then
-	storage_mkdir ${SPARK_HISTORY_SERVER_DIR}
-	storage_chmod -R 777 ${SPARK_HISTORY_SERVER_DIR}
+	storage_mkdir "$SPARK_HISTORY_SERVER_DIR"
+	storage_chmod -R 777 "$SPARK_HISTORY_SERVER_DIR"
 
     #Spark history server
-	$SPARK_HOME/sbin/start-history-server.sh
+	"$SPARK_HOME/sbin/start-history-server.sh"
 fi
