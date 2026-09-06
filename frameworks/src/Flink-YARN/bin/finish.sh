217bcec619483d1be3e603675161509bede996ce
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-06 13:13:22 +0200

    Fix quoting in finish.sh for yarn-session.sh

--- frameworks/src/Flink-YARN/bin/finish.sh
+++ frameworks/src/Flink-YARN/bin/finish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "stop" | $FLINK_HOME/bin/yarn-session.sh -id $YARN_APP_ID
+echo "stop" | "$FLINK_HOME/bin/yarn-session.sh" -id $YARN_APP_ID
 
-$COMMON_SRC_DIR/bin/stop_hadoop_yarn.sh
+"$COMMON_SRC_DIR/bin/stop_hadoop_yarn.sh"
