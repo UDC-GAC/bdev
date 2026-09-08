6c5c2b163541f2e113ef46810f315a1a26189f9d
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2026-09-08 15:43:47 +0200

    Fix symlink creation

--- frameworks/src/RDMA-Hadoop-3/bin/start.sh
+++ frameworks/src/RDMA-Hadoop-3/bin/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Fix for Java 9+
-ln -s "$SOLUTION_DIR/lib/-javax.activation-api-*.jar" "$SOLUTION_LIB_DIR"
+for jar in "$SOLUTION_DIR"/lib/javax.activation-api-*.jar; do
+    if [[ -f "$jar" ]]; then
+        ln -sf "$jar" "$SOLUTION_LIB_DIR"/
+    fi
+done
 
 . "$COMMON_HADOOP_DIR/bin/start.sh"
