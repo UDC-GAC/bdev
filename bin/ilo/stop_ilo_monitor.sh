7b88da6b09e1e46553dd4b697e2ab2077136f658
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-10 22:11:24 +0200

    Modernize syntax

--- bin/ilo/stop_ilo_monitor.sh
+++ bin/ilo/stop_ilo_monitor.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-PROCESSES=`$SSH_CMD $ILO_MASTER 'ps -elf' | grep "ilo_monitor" | grep -v "stop_ilo_monitor" | tr -s " "`
+PROCESSES=$($SSH_CMD $ILO_MASTER 'ps -elf' | grep "ilo_monitor" | grep -v "stop_ilo_monitor" | tr -s " ")
 echo "$PROCESSES" >> ${POWERLOGDIR}/log
-PIDS=`echo "$PROCESSES" | cut -f 4 -d " "`
+PIDS=$(echo "$PROCESSES" | cut -f 4 -d " ")
 
-for PID in $PIDS
-do 
+for PID in $PIDS; do
 	$SSH_CMD $ILO_MASTER "kill -9 $PID" >> ${POWERLOGDIR}/log 2>&1
 done
