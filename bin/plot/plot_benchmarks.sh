a90654b40bf75e528c4c1bcbac26b07dc5e532b0
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-17 22:18:12 +0200

    Add conditional message for performance plot generation

--- bin/plot/plot_benchmarks.sh
+++ bin/plot/plot_benchmarks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[[ "$GNUPLOT_BIN" != "null" ]] && m_echo "Generating performance plots"
+
 if [[ "$BENCHMARK" == "testdfsio" ]]; then
 	bash "$PLOT_HOME/plot_benchmark_testdfsio.sh"
 else
