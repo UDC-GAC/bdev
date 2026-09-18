b90be498460545f8bd61443856c68de0d603ecbb
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-18 23:29:43 +0200

    Refactor quoting in start_oprofile_monitor.sh
    Updated quoting style for variable expansions and improved readability.

--- bin/oprofile/start_oprofile_monitor.sh
+++ bin/oprofile/start_oprofile_monitor.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-mkdir -p $OPROFILELOGDIR
+mkdir -p "$OPROFILELOGDIR"
 NODE_NUMBER=0
 
 # Deduplicate nodes in case the master is also a worker
 UNIQUE_NODES=$(printf '%s\n' $MASTERNODE $WORKERNODES | sort -u)
 
 for NODE in $UNIQUE_NODES; do
-	OPROFILENODEDIR=${OPROFILELOGDIR}/node-${NODE_NUMBER}
-	mkdir -p ${OPROFILENODEDIR}
+	OPROFILENODEDIR="${OPROFILELOGDIR}/node-${NODE_NUMBER}"
+	mkdir -p "$OPROFILENODEDIR"
 	echo "Starting oprofile monitor in ${NODE}, storing data on ${OPROFILENODEDIR}" >> ${OPROFILELOGDIR}/log 2>&1
-	nohup $SSH_CMD $NODE "export OPROFILELOGFILE=${OPROFILENODEDIR}/oprofile; \
-		export OPROFILE_BIN=${OPROFILE_BIN}; \
-		export OPROFILE_EVENTS=${OPROFILE_EVENTS}; \
-		bash ${OPROFILE_HOME}/oprofile_monitor.sh" > ${OPROFILENODEDIR}/oprofile.out 2>&1 &
+	nohup $SSH_CMD $NODE "export OPROFILELOGFILE='${OPROFILENODEDIR}/oprofile'; \
+		export OPROFILE_BIN='${OPROFILE_BIN}'; \
+		export OPROFILE_EVENTS='${OPROFILE_EVENTS:-}'; \
+		bash '${OPROFILE_HOME}/oprofile_monitor.sh'" > "${OPROFILENODEDIR}/oprofile.out" 2>&1 &
 	
 	NODE_NUMBER=$(( $NODE_NUMBER + 1 ))
 done
