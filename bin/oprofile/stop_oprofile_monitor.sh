ab73689fcb316c91a9b71ceb6054333f54811765
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-18 23:30:37 +0200

    Simplify stopping OProfile monitor with parallel SSH

--- bin/oprofile/stop_oprofile_monitor.sh
+++ bin/oprofile/stop_oprofile_monitor.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# Deduplicate nodes in case the master is also a worker
-UNIQUE_NODES=$(printf '%s\n' $MASTERNODE $WORKERNODES | sort -u)
-
-for NODE in $UNIQUE_NODES
-do
-	$SSH_CMD $NODE "killall -u $USER -q -SIGINT $OPROFILE_BIN"
-done
+parallel_ssh "killall -u '$USER' -q -SIGINT '$OPROFILE_BIN'" "${OPROFILELOGDIR}/log"
