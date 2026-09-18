52346b92c0bc8a3abd1ef3e8baec4e5593fb32e2
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 00:56:25 +0200

    Refactor ilo_monitor.sh for improved argument handling

--- bin/ilo/ilo_monitor.sh
+++ bin/ilo/ilo_monitor.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-       echo "$0 node"
-       exit -1
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $0 <node_ip_or_hostname> [node_number]" >&2
+    exit 1
 fi
 
-NODE_IP=$1
-FINAL_DIGIT=$(echo $NODE_IP | cut -d "." -f 4)
+TARGET_NODE="$1"
+NODE_NUMBER="${2:-0}"
+
+# Resolve the IP if a hostname is provided, ensuring the last octet is always numeric
+NODE_IP="$TARGET_NODE"
+if [[ ! "$TARGET_NODE" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    RESOLVED_IP=$(getent ahostsv4 "$TARGET_NODE" 2>/dev/null | awk '{print $1; exit}')
+    [[ -n "$RESOLVED_IP" ]] && NODE_IP="$RESOLVED_IP"
+fi
+
+FINAL_DIGIT="${NODE_IP##*.}"
 ILO_IP="${ILO_BASE_IP}.${FINAL_DIGIT}"
 
-echo -e "Time(s) Power(w) (node-${NODE_NUMBER}, IP: ${NODE_IP}, iLO IP: ${ILO_IP})"
+printf "Time(s)\tPower(w) (node-%s, Host: %s, IP: %s, iLO IP: %s)\n" \
+    "$NODE_NUMBER" "$TARGET_NODE" "$NODE_IP" "$ILO_IP"
 
-BEGIN_EPOCH=`date +%s`
+BEGIN_EPOCH=$(date +%s)
 TARGET_EPOCH=$BEGIN_EPOCH
 
-while true
-do
-	LOOP_EPOCH=`date +%s`
-	TARGET_EPOCH=$(( $TARGET_EPOCH + $ILO_SECONDS_INTERVAL ))
-	PRESENT_POWER_READINGS=""
-	
-	NODE_POWER_READINGS=`$ILO_CONFIG_SCRIPT -s $ILO_IP -f $ILO_POWER_SCRIPT`
-	NODE_PRESENT_POWER_READING=$(echo "$NODE_POWER_READINGS" | grep "PRESENT_POWER_READING")
-	NODE_PRESENT_POWER_READING=${NODE_PRESENT_POWER_READING##*<PRESENT_POWER_READING VALUE=\"}
-	NODE_PRESENT_POWER_READING=${NODE_PRESENT_POWER_READING%\" UNIT=\"Watts\"/>*}
-	PRESENT_POWER_READINGS="$PRESENT_POWER_READINGS $NODE_PRESENT_POWER_READING"
+while true; do
+    LOOP_EPOCH=$(date +%s)
+    TARGET_EPOCH=$(( TARGET_EPOCH + ILO_SECONDS_INTERVAL ))
 
-	CURRENT_EPOCH=$(date +%s)
-	SLEEP_SECONDS=$(( $TARGET_EPOCH - $CURRENT_EPOCH ))
-	CURRENT_TIME=$(( $LOOP_EPOCH - $BEGIN_EPOCH ))
-	echo -e "$CURRENT_TIME \t $PRESENT_POWER_READINGS"
-	
-	if [[ $SLEEP_SECONDS -gt 0 ]]; then
-		sleep $SLEEP_SECONDS
-	fi
+    NODE_POWER_READINGS=$("$ILO_CONFIG_SCRIPT" -s "$ILO_IP" -f "$ILO_POWER_SCRIPT" 2>/dev/null)
+
+    # Extract the value of PRESENT_POWER_READING
+    POWER_VAL=$(grep "PRESENT_POWER_READING" <<< "$NODE_POWER_READINGS" || true)
+    POWER_VAL="${POWER_VAL##*<PRESENT_POWER_READING VALUE=\"}"
+    POWER_VAL="${POWER_VAL%\" UNIT=\"Watts\"/>*}"
+
+    CURRENT_EPOCH=$(date +%s)
+    SLEEP_SECONDS=$(( TARGET_EPOCH - CURRENT_EPOCH ))
+    CURRENT_TIME=$(( LOOP_EPOCH - BEGIN_EPOCH ))
+
+    printf "%d\t%s\n" "$CURRENT_TIME" "${POWER_VAL:-0}"
+
+    if (( SLEEP_SECONDS > 0 )); then
+        sleep "$SLEEP_SECONDS"
+    fi
 done
