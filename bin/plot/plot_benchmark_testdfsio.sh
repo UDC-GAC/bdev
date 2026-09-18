7c7930f536498ecb6c6913f92911dbcac3cdeb02
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2026-09-18 11:34:42 +0200

    Fix Y range for some plots

--- bin/plot/plot_benchmark_testdfsio.sh
+++ bin/plot/plot_benchmark_testdfsio.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
 
-FILES=`find $REPORT_DIR -wholename */testdfsio_*/output`
-
-REPORT_CONTENTS=""
-
-for FILE in $FILES
-do
-	FRAMEWORK=$(basename $( dirname $( dirname $FILE)))
-	CLUSTER_SIZE=$(basename $(dirname $( dirname $(dirname $FILE))))
-	
-	CONTENT=`cat $FILE | grep "TestDFSIO ----- : write" -A 8` 
-	if [[ -n "$CONTENT" ]]
-	then
-		LINE=`echo "$CONTENT" | grep Throughput`
-		THROUGHPUT=`echo "$LINE" | tr -s " " | cut -f 7 -d " "`
-		TESTDFSIO_WRITE_CONTENTS=`echo "$TESTDFSIO_WRITE_CONTENTS""$CLUSTER_SIZE \t $FRAMEWORK \t ${BENCHMARK}_write \t "$THROUGHPUT "\n"`
-	fi
-
-	CONTENT=`cat $FILE | grep "TestDFSIO ----- : read" -A 8` 
-	if [[ -n "$CONTENT" ]]
-	then
-		LINE=`echo "$CONTENT" | grep Throughput`
-		THROUGHPUT=`echo "$LINE" | tr -s " " | cut -f 7 -d " "`
-		TESTDFSIO_READ_CONTENTS=`echo "$TESTDFSIO_READ_CONTENTS""$CLUSTER_SIZE \t $FRAMEWORK \t ${BENCHMARK}_read \t "$THROUGHPUT "\n"`
-	fi
-
-done
-
+TESTDFSIO_WRITE_CONTENTS=""
+TESTDFSIO_READ_CONTENTS=""
 YLABEL="Throughput (MB/s)"
 
-BENCHMARK_TAG="TestDFSIO Write Throughput"
-REPORT_CONTENTS=$(echo -e "$TESTDFSIO_WRITE_CONTENTS")
-DAT_FILE="$PLOT_DIR/${BENCHMARK}_write.dat"
-PLOT_FILE="$PLOT_DIR/${BENCHMARK}_write.eps"
-. "$PLOT_HOME/gen_plot.sh"
+while IFS= read -r FILE; do
+	dir="${FILE%/*}"          # Removes /output -> .../cluster/framework/testdfsio_*
+	dir="${dir%/*}"           # Removes /testdfsio_* -> .../cluster/framework
+	FRAMEWORK="${dir##*/}"    # Extracts the framework
+	dir="${dir%/*}"           # Removes /framework -> .../cluster
+	CLUSTER_SIZE="${dir##*/}" # Extracts the cluster size
 
-BENCHMARK_TAG="TestDFSIO Read Throughput"
-REPORT_CONTENTS=$(echo -e "$TESTDFSIO_READ_CONTENTS")
-DAT_FILE="$PLOT_DIR/${BENCHMARK}_read.dat"
-PLOT_FILE="$PLOT_DIR/${BENCHMARK}_read.eps"
-. "$PLOT_HOME/gen_plot.sh"
+	# Clean extraction of throughput in a single pass (the number is the last field, $NF)
+	WRITE_TP=$(awk '/TestDFSIO ----- : write/{f=1} f && /Throughput/{print $NF; exit}' "$FILE")
+	if [[ -n "$WRITE_TP" ]]; then
+		TESTDFSIO_WRITE_CONTENTS+="${CLUSTER_SIZE}"$'\t'"${FRAMEWORK}"$'\t'"${BENCHMARK}_write"$'\t'"${WRITE_TP}"$'\n'
+	fi
+
+	READ_TP=$(awk '/TestDFSIO ----- : read/{f=1} f && /Throughput/{print $NF; exit}' "$FILE")
+	if [[ -n "$READ_TP" ]]; then
+		TESTDFSIO_READ_CONTENTS+="${CLUSTER_SIZE}"$'\t'"${FRAMEWORK}"$'\t'"${BENCHMARK}_read"$'\t'"${READ_TP}"$'\n'
+	fi
+done < <(find "$REPORT_DIR" -type f -wholename "*/testdfsio_*/output")
+
+if [[ -n "$TESTDFSIO_WRITE_CONTENTS" ]]; then
+	BENCHMARK_TAG="TestDFSIO Write Throughput"
+	REPORT_CONTENTS="$TESTDFSIO_WRITE_CONTENTS"
+	DAT_FILE="$PLOT_DIR/${BENCHMARK}_write.dat"
+	PLOT_FILE="$PLOT_DIR/${BENCHMARK}_write.eps"
+	. "$PLOT_HOME/gen_plot.sh"
+fi
+
+if [[ -n "$TESTDFSIO_READ_CONTENTS" ]]; then
+	BENCHMARK_TAG="TestDFSIO Read Throughput"
+	REPORT_CONTENTS="$TESTDFSIO_READ_CONTENTS"
+	DAT_FILE="$PLOT_DIR/${BENCHMARK}_read.dat"
+	PLOT_FILE="$PLOT_DIR/${BENCHMARK}_read.eps"
+	. "$PLOT_HOME/gen_plot.sh"
+fi
