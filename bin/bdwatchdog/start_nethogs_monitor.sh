59b41e967014ae536b9abe997ace544e0639271a
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 00:31:05 +0200

    Fix command substitution in start_nethogs_monitor.sh

--- bin/bdwatchdog/start_nethogs_monitor.sh
+++ bin/bdwatchdog/start_nethogs_monitor.sh
@@ -6,4 +6,4 @@ if [[ ! -f "${BDW_LOG_DIR}/config.sh" ]]; then
     bash "${BDWATCHDOG_HOME}/gen-config.sh" > "${BDW_LOG_DIR}/config.sh"
 fi
 
-parallel_ssh ". '${BDW_LOG_DIR}/config.sh'; bash '${BDWATCHDOG_HOME}/nethogs-config.sh'; '${PYTHON_BIN}' '${BDWATCHDOG_DAEMONS_DIR}/nethogs.py' start" "${BDW_LOG_DIR}/nethogs_log"
+parallel_ssh ". '${BDW_LOG_DIR}/config.sh'; . '${BDWATCHDOG_HOME}/nethogs-config.sh'; '${PYTHON_BIN}' '${BDWATCHDOG_DAEMONS_DIR}/nethogs.py' start" "${BDW_LOG_DIR}/nethogs_log"
