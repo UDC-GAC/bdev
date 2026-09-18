4b848dac878b85e3f6e393f151370c33aa6d9553
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 00:25:03 +0200

    Refactor nethogs monitor script for efficiency

--- bin/bdwatchdog/start_nethogs_monitor.sh
+++ bin/bdwatchdog/start_nethogs_monitor.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-if [[ ! -d "${BDW_LOG_DIR}" ]]; then
-	mkdir -p ${BDW_LOG_DIR}
-	mkdir -p ${BDW_LOG_DIR}/java_mappings
+mkdir -p "${BDW_LOG_DIR}/java_mappings"
 
-	if [[ ! -f "${BDW_LOG_DIR}/config.sh" ]]; then
-		bash ${BDWATCHDOG_HOME}/gen-config.sh > ${BDW_LOG_DIR}/config.sh
-	fi
+if [[ ! -f "${BDW_LOG_DIR}/config.sh" ]]; then
+    bash "${BDWATCHDOG_HOME}/gen-config.sh" > "${BDW_LOG_DIR}/config.sh"
 fi
 
-# Deduplicate nodes in case the master is also a worker
-UNIQUE_NODES=$(printf '%s\n' $MASTERNODE $WORKERNODES | sort -u)
-
-for NODE in $UNIQUE_NODES; do
-	echo "Starting nethogs daemon in ${NODE}" >> ${BDW_LOG_DIR}/nethogs_log 2>&1
-	$SSH_CMD $NODE ". ${BDW_LOG_DIR}/config.sh; \
-		${BDWATCHDOG_HOME}/nethogs-config.sh; \
-		${PYTHON_BIN} ${BDWATCHDOG_DAEMONS_DIR}/nethogs.py start" >> ${BDW_LOG_DIR}/nethogs_log 2>&1
-done
+parallel_ssh ". '${BDW_LOG_DIR}/config.sh'; bash '${BDWATCHDOG_HOME}/nethogs-config.sh'; '${PYTHON_BIN}' '${BDWATCHDOG_DAEMONS_DIR}/nethogs.py' start" "${BDW_LOG_DIR}/nethogs_log"
