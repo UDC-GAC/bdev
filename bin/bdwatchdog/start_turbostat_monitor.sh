6b1482415836699aabdd6effb18848279820e055
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 00:31:19 +0200

    Fix command syntax for turbostat daemon startup

--- bin/bdwatchdog/start_turbostat_monitor.sh
+++ bin/bdwatchdog/start_turbostat_monitor.sh
@@ -6,5 +6,4 @@ if [[ ! -f "${BDW_LOG_DIR}/config.sh" ]]; then
     bash "${BDWATCHDOG_HOME}/gen-config.sh" > "${BDW_LOG_DIR}/config.sh"
 fi
 
-# Start turbostat daemon across all nodes in parallel
-parallel_ssh ". '${BDW_LOG_DIR}/config.sh'; bash '${BDWATCHDOG_HOME}/turbostat-config.sh'; '${PYTHON_BIN}' '${BDWATCHDOG_DAEMONS_DIR}/turbostat.py' start" "${BDW_LOG_DIR}/turbostat_log"
+parallel_ssh ". '${BDW_LOG_DIR}/config.sh'; . '${BDWATCHDOG_HOME}/turbostat-config.sh'; '${PYTHON_BIN}' '${BDWATCHDOG_DAEMONS_DIR}/turbostat.py' start" "${BDW_LOG_DIR}/turbostat_log"
