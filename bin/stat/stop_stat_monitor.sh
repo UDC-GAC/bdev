56cefaf49f39544c9b752e74c0599e2581d19205
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-18 23:37:00 +0200

    Simplify stopping stat monitor using parallel SSH
    Replaced unique node stopping logic with parallel SSH command to kill processes.

--- bin/stat/stop_stat_monitor.sh
+++ bin/stat/stop_stat_monitor.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Deduplicate nodes in case the master is also a worker
-UNIQUE_NODES=$(printf '%s\n' $MASTERNODE $WORKERNODES | sort -u)
-
-for NODE in $UNIQUE_NODES; do
-	echo "Stopping dool monitor in ${NODE}" >> ${STATLOGDIR}/log 2>&1
-	$SSH_CMD $NODE "export DOOL_COMMAND_NAME=${DOOL_COMMAND_NAME};export PYTHON_BIN=${PYTHON_BIN};\
-		${STAT_HOME}/kill_stat_monitor.sh" >> ${STATLOGDIR}/log 2>&1
-done
+parallel_ssh "pkill -u '$USER' -9 -f '(^|.*/)python.*${DOOL_COMMAND_NAME}' 2>/dev/null || true" "${STATLOGDIR}/log"
