9f70db38105e1e31cda7407f51d30c274355eb09
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 00:45:33 +0200

    Enhance stop_stat_monitor.sh with a stopping message
    Added a message to indicate the stopping of the dool monitor.

--- bin/stat/stop_stat_monitor.sh
+++ bin/stat/stop_stat_monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-parallel_ssh "pkill -u '$USER' -9 -f '(^|.*/)python.*${DOOL_COMMAND_NAME}' 2>/dev/null || true" "${STATLOGDIR}/log"
+parallel_ssh "pkill -u '$USER' -9 -f '(^|.*/)python.*${DOOL_COMMAND_NAME}' 2>/dev/null || true" "${STATLOGDIR}/log" "Stopping dool monitor"
