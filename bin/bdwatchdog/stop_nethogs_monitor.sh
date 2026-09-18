e4e9b0091bd958e676448ee965bf55fb82290d16
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 00:43:38 +0200

    Add stop message for nethogs monitor
    Added a message to indicate the stopping of the nethogs monitor.

--- bin/bdwatchdog/stop_nethogs_monitor.sh
+++ bin/bdwatchdog/stop_nethogs_monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-parallel_ssh ". '${BDW_LOG_DIR}/config.sh'; '${PYTHON_BIN}' '${BDWATCHDOG_DAEMONS_DIR}/nethogs.py' stop" "${BDW_LOG_DIR}/nethogs_log"
+parallel_ssh ". '${BDW_LOG_DIR}/config.sh'; '${PYTHON_BIN}' '${BDWATCHDOG_DAEMONS_DIR}/nethogs.py' stop" "${BDW_LOG_DIR}/nethogs_log" "Stopping nethogs monitor"
