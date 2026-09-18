e1500a7d01dc26f102eb5bd993eebcf4b892cda8
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 01:16:47 +0200

    Refactor start_yarn_2.sh for subshell execution
    Refactor start_yarn_2.sh to run HDFS and YARN components in subshells for better process management. Added wait for all components to complete deployment.

--- src/common/hadoop/bin/start_yarn_2.sh
+++ src/common/hadoop/bin/start_yarn_2.sh
@@ -1,39 +1,52 @@
 #!/bin/bash
 
+# HDFS
 if [[ "${STORAGE_BACKEND,,}" == "hdfs" ]]; then
-	if [[ $FORCE_FORMAT_HDFS == "true" ]]; then
-		#Format HDFS
-		HDFS_FORMAT_LOG=$FRAMEWORK_REPORT_DIR/hdfs-format.log
-		m_echo "Formatting HDFS, logging to $HDFS_FORMAT_LOG"
-		$HDFS_CONFIG "$HADOOP_CONF_DIR" namenode -format -force -clusterID CID-bdev > "$HDFS_FORMAT_LOG" 2>&1
-	fi
+    (
+        if [[ "$FORCE_FORMAT_HDFS" == "true" ]]; then
+			#Format HDFS
+            HDFS_FORMAT_LOG="$FRAMEWORK_REPORT_DIR/hdfs-format.log"
+            m_echo "Formatting HDFS, logging to $HDFS_FORMAT_LOG"
+            $HDFS_CONFIG "$HADOOP_CONF_DIR" namenode -format -force -clusterID CID-bdev > "$HDFS_FORMAT_LOG" 2>&1
+        fi
 
-	#Namenode and Secondary NameNode (optional)
-	m_echo "Starting NameNode and DataNodes"
-	"$HADOOP_HOME/sbin/hadoop-daemon.sh" --config "$HADOOP_CONF_DIR" --script hdfs start namenode
+        m_echo "Starting NameNode and DataNodes"
+        "$HADOOP_HOME/sbin/hadoop-daemon.sh" --config "$HADOOP_CONF_DIR" --script hdfs start namenode
 
-	if [[ $SECONDARY_NAMENODE == "true" ]]; then
-		#Secondary NameNode
-		"$HADOOP_HOME/sbin/hadoop-daemon.sh" --config "$HADOOP_CONF_DIR" --script hdfs start secondarynamenode
-	fi
+        if [[ "$SECONDARY_NAMENODE" == "true" ]]; then
+			#Secondary NameNode
+            "$HADOOP_HOME/sbin/hadoop-daemons.sh" --config "$HADOOP_CONF_DIR" --script hdfs start secondarynamenode &
+        fi
 
-	#Datanodes
-	"$HADOOP_HOME/sbin/hadoop-daemons.sh" --config "$HADOOP_CONF_DIR" --script hdfs start datanode
+		#Datanodes
+        "$HADOOP_HOME/sbin/hadoop-daemons.sh" --config "$HADOOP_CONF_DIR" --script hdfs start datanode
+    ) &
 fi
 
-#Resourcemanager & Nodemanagers
-m_echo "Starting Resourcemanager and Nodemanagers"
-"$HADOOP_HOME/sbin/yarn-daemon.sh" --config "$HADOOP_CONF_DIR" start resourcemanager
-"$HADOOP_HOME/sbin/yarn-daemons.sh" --config "$HADOOP_CONF_DIR" start nodemanager
+# YARN
+(
+	#Resourcemanager & Nodemanagers
+    m_echo "Starting ResourceManager and NodeManagers"
+    "$HADOOP_HOME/sbin/yarn-daemon.sh" --config "$HADOOP_CONF_DIR" start resourcemanager
+    "$HADOOP_HOME/sbin/yarn-daemons.sh" --config "$HADOOP_CONF_DIR" start nodemanager
+) &
 
-if [[ $TIMELINE_SERVER == "true" ]]; then
-	#YARN Timeline server
-	m_echo "Starting YARN Timeline server"
-	"$HADOOP_HOME/sbin/yarn-daemon.sh" --config "$HADOOP_CONF_DIR" start timelineserver
+# Auxiliary servers
+if [[ "$TIMELINE_SERVER" == "true" ]]; then
+    (
+		#YARN Timeline server
+        m_echo "Starting YARN Timeline server"
+        "$HADOOP_HOME/sbin/yarn-daemon.sh" --config "$HADOOP_CONF_DIR" start timelineserver
+    ) &
 fi
 
-if [[ $MR_JOBHISTORY_SERVER == "true" ]]; then
-	#MapReduce history server
-	m_echo "Starting MapReduce history server"
-	"$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh" --config $HADOOP_CONF_DIR start historyserver
+if [[ "$MR_JOBHISTORY_SERVER" == "true" ]]; then
+    (
+		#MapReduce history server
+        m_echo "Starting MapReduce history server"
+        "$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh" --config "$HADOOP_CONF_DIR" start historyserver
+    ) &
 fi
+
+# Wait for all components to complete their deployment
+wait
