3d92476887beb8081e3f15a7dab6ed3fa2afd999
Roberto R. Exposito <rreye@udc.es>
2026-09-18 23:41:47 +0200

    Remove unused script

--- bin/stat/kill_stat_monitor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOOL_PID=$(ps -elf | grep ${PYTHON_BIN} | grep ${DOOL_COMMAND_NAME} | grep -v "ssh" | grep -v "export" | awk '{print $4}')
-
-if [[ -n "$DOOL_PID" ]]; then
-	echo "$HOSTNAME: cleaning up ${DOOL_COMMAND_NAME} with PID $DOOL_PID"
-	kill -9 $DOOL_PID
-fi
