04f1e8191082e2360c2a6680c55bf7f99d7db0c3
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 00:53:32 +0200

    Update stop_ilo_monitor.sh to use pkill for termination
    Refactor process termination for iLO monitors to use pkill.

--- bin/ilo/stop_ilo_monitor.sh
+++ bin/ilo/stop_ilo_monitor.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-PROCESSES=$($SSH_CMD $ILO_MASTER 'ps -elf' | grep "ilo_monitor" | grep -v "stop_ilo_monitor" | tr -s " ")
-echo "$PROCESSES" >> ${POWERLOGDIR}/log
-PIDS=$(echo "$PROCESSES" | cut -f 4 -d " ")
+if [[ "$ILO_MASTER" == "localhost" || -z "$ILO_MASTER" ]]; then
+    export ILO_MASTER="$MASTERNODE"
+fi
 
-for PID in $PIDS; do
-	$SSH_CMD $ILO_MASTER "kill -9 $PID" >> ${POWERLOGDIR}/log 2>&1
-done
+m_echo "Stopping iLO monitors on $ILO_MASTER"
+echo "Stopping iLO monitors on $ILO_MASTER" >> "${POWERLOGDIR}/log" 2>&1
+
+$SSH_CMD "$ILO_MASTER" "pkill -9 -f 'ilo_monitor.sh' 2>/dev/null || true" >> "${POWERLOGDIR}/log" 2>&1
