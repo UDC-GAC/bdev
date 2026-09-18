a732843dcfcfaf20de3d8884f1131e0bb1b8d26b
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 00:26:19 +0200

    Update turbostat monitor script for parallel execution
    Refactor turbostat monitor script to use parallel SSH for starting daemons.

--- bin/bdwatchdog/start_turbostat_monitor.sh
+++ bin/bdwatchdog/start_turbostat_monitor.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-if [[ ! -d "${BDW_LOG_DIR}" ]]; then
-	mkdir -p ${BDW_LOG_DIR}
-	mkdir -p ${BDW_LOG_DIR}/java_mappings
+mkdir -p "${BDW_LOG_DIR}/java_mappings"
 
-	if [[ ! -f "${BDW_LOG_DIR}/config.sh" ]]; then
-		bash ${BDWATCHDOG_HOME}/gen-config.sh > ${BDW_LOG_DIR}/config.sh
-	fi
+if [[ ! -f "${BDW_LOG_DIR}/config.sh" ]]; then
+    bash "${BDWATCHDOG_HOME}/gen-config.sh" > "${BDW_LOG_DIR}/config.sh"
 fi
 
-# Deduplicate nodes in case the master is also a worker
-UNIQUE_NODES=$(printf '%s\n' $MASTERNODE $WORKERNODES | sort -u)
-
-for NODE in $UNIQUE_NODES; do
-	echo "Starting turbostat daemon in ${NODE}" >> ${BDW_LOG_DIR}/turbostat_log 2>&1
-	$SSH_CMD $NODE ". ${BDW_LOG_DIR}/config.sh; \
-		${BDWATCHDOG_HOME}/turbostat-config.sh; \
-   		${PYTHON_BIN} ${BDWATCHDOG_DAEMONS_DIR}/turbostat.py start" >> ${BDW_LOG_DIR}/turbostat_log 2>&1
-done
+# Start turbostat daemon across all nodes in parallel
+parallel_ssh ". '${BDW_LOG_DIR}/config.sh'; bash '${BDWATCHDOG_HOME}/turbostat-config.sh'; '${PYTHON_BIN}' '${BDWATCHDOG_DAEMONS_DIR}/turbostat.py' start" "${BDW_LOG_DIR}/turbostat_log"
