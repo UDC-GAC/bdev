ce6f1f8d81d3a44338ca6b5b6bb3b64edfd3773d
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 00:45:06 +0200

    Fix typo in stopping message for RAPL monitor

--- bin/rapl/stop_rapl_monitor.sh
+++ bin/rapl/stop_rapl_monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-parallel_ssh "killall -u '$USER' -q -9 '$RAPL_COMMAND_NAME'" "${RAPLLOGDIR}/log" "Stopping rapl monitor"
+parallel_ssh "killall -u '$USER' -q -9 '$RAPL_COMMAND_NAME'" "${RAPLLOGDIR}/log" "Stopping RAPL monitor"
