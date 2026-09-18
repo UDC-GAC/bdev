e9b95880ff0140e20a6c4972ae8a87d78b35f1ac
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 00:27:05 +0200

    Refactor turbostat stop script to use parallel SSH

--- bin/bdwatchdog/stop_turbostat_monitor.sh
+++ bin/bdwatchdog/stop_turbostat_monitor.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Deduplicate nodes in case the master is also a worker
-UNIQUE_NODES=$(printf '%s\n' $MASTERNODE $WORKERNODES | sort -u)
-
-for NODE in $UNIQUE_NODES; do
-	echo "Stopping turbostat daemon in ${NODE}" >> ${BDW_LOG_DIR}/turbostat_log 2>&1
-	$SSH_CMD $NODE ". ${BDW_LOG_DIR}/config.sh; \
-		${PYTHON_BIN} ${BDWATCHDOG_DAEMONS_DIR}/turbostat.py stop" >> ${BDW_LOG_DIR}/turbostat_log 2>&1
-done
+parallel_ssh ". '${BDW_LOG_DIR}/config.sh'; '${PYTHON_BIN}' '${BDWATCHDOG_DAEMONS_DIR}/turbostat.py' stop" "${BDW_LOG_DIR}/turbostat_log"
