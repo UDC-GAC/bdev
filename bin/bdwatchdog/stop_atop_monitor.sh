bf972396298d9ebe723bfea431d21f46642ed386
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 00:39:26 +0200

    Add message to atop monitor stop command

--- bin/bdwatchdog/stop_atop_monitor.sh
+++ bin/bdwatchdog/stop_atop_monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-parallel_ssh ". '${BDW_LOG_DIR}/config.sh'; '${PYTHON_BIN}' '${BDWATCHDOG_DAEMONS_DIR}/atop.py' stop" "${BDW_LOG_DIR}/atop_log"
+parallel_ssh ". '${BDW_LOG_DIR}/config.sh'; '${PYTHON_BIN}' '${BDWATCHDOG_DAEMONS_DIR}/atop.py' stop" "${BDW_LOG_DIR}/atop_log" "Stopping atop monitor"
