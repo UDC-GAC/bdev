feed9dea1a221c2203a35d2a1ec26c16a86fc170
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-19 00:50:31 +0200

    Fix quoting and formatting in start_ilo_monitor.sh

--- bin/ilo/start_ilo_monitor.sh
+++ bin/ilo/start_ilo_monitor.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-mkdir -p $POWERLOGDIR
+mkdir -p "$POWERLOGDIR"
 
-if [[ $ILO_MASTER == "localhost" ]]; then
-        export ILO_MASTER=$MASTERNODE
+if [[ "$ILO_MASTER" == "localhost" || -z "$ILO_MASTER" ]]; then
+    export ILO_MASTER="$MASTERNODE"
 fi
 
 m_echo "iLO master: $ILO_MASTER"
 
-nohup $SSH_CMD $ILO_MASTER \
-"export POWERLOGDIR='$POWERLOGDIR';\
-export WORKERNODES='$WORKERNODES';\
-export MASTERNODE='$MASTERNODE';\
-export ILO_HOME='$ILO_HOME';\
-export ILO_POWER_SCRIPT='$ILO_POWER_SCRIPT';\
-export ILO_CONFIG_SCRIPT='$ILO_CONFIG_SCRIPT';\
-export ILO_SECONDS_INTERVAL='$ILO_SECONDS_INTERVAL';\
-export ILO_BASE_IP='$ILO_BASE_IP';\
-bash $ILO_HOME/create_ilo_monitors.sh" >/dev/null 2>&1 &
+nohup $SSH_CMD "$ILO_MASTER" \
+    "export POWERLOGDIR='$POWERLOGDIR'; \
+     export WORKERNODES='$WORKERNODES'; \
+     export MASTERNODE='$MASTERNODE'; \
+     export ILO_HOME='$ILO_HOME'; \
+     export ILO_POWER_SCRIPT='$ILO_POWER_SCRIPT'; \
+     export ILO_CONFIG_SCRIPT='$ILO_CONFIG_SCRIPT'; \
+     export ILO_SECONDS_INTERVAL='$ILO_SECONDS_INTERVAL'; \
+     export ILO_BASE_IP='$ILO_BASE_IP'; \
+     bash '$ILO_HOME/create_ilo_monitors.sh'" >/dev/null 2>&1 &
