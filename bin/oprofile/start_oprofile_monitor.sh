5060b133341491502fdd79d6880ded83e863f1c1
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-18 23:42:38 +0200

    Enhance oprofile monitor script with logging
    Added logging for starting oprofile monitor and output redirection.

--- bin/oprofile/start_oprofile_monitor.sh
+++ bin/oprofile/start_oprofile_monitor.sh
@@ -10,6 +10,7 @@ for NODE in $UNIQUE_NODES; do
 	OPROFILENODEDIR="${OPROFILELOGDIR}/node-${NODE_NUMBER}"
 	mkdir -p "$OPROFILENODEDIR"
 	echo "Starting oprofile monitor in ${NODE}, storing data on ${OPROFILENODEDIR}" >> "${OPROFILELOGDIR}/log" 2>&1
+
 	nohup $SSH_CMD $NODE "export OPROFILELOGFILE='${OPROFILENODEDIR}/oprofile'; \
 		export OPROFILE_BIN='${OPROFILE_BIN}'; \
 		export OPROFILE_EVENTS='${OPROFILE_EVENTS:-}'; \
