68c1039f3d3859978b78c4bbc9a9a348a3cd46ee
Roberto R. Expósito <34656045+rreye@users.noreply.github.com>
2026-09-18 23:30:06 +0200

    Fix log file redirection in start_oprofile_monitor.sh

--- bin/oprofile/start_oprofile_monitor.sh
+++ bin/oprofile/start_oprofile_monitor.sh
@@ -9,7 +9,7 @@ UNIQUE_NODES=$(printf '%s\n' $MASTERNODE $WORKERNODES | sort -u)
 for NODE in $UNIQUE_NODES; do
 	OPROFILENODEDIR="${OPROFILELOGDIR}/node-${NODE_NUMBER}"
 	mkdir -p "$OPROFILENODEDIR"
-	echo "Starting oprofile monitor in ${NODE}, storing data on ${OPROFILENODEDIR}" >> ${OPROFILELOGDIR}/log 2>&1
+	echo "Starting oprofile monitor in ${NODE}, storing data on ${OPROFILENODEDIR}" >> "${OPROFILELOGDIR}/log" 2>&1
 	nohup $SSH_CMD $NODE "export OPROFILELOGFILE='${OPROFILENODEDIR}/oprofile'; \
 		export OPROFILE_BIN='${OPROFILE_BIN}'; \
 		export OPROFILE_EVENTS='${OPROFILE_EVENTS:-}'; \
